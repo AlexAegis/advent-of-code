dccf7389d2589ac39bf3b9f7bda2215b4877b246
Győri Sándor <alexaegis@gmail.com>
2018-12-11 12:02:58 +0100

    1 task done

--- day1/main.ts
+++ day1/main.ts
@@ -1,1 +1,21 @@
-console.log('shiet');
+import { createReadStream } from 'fs';
+import { Interface, createInterface } from 'readline';
+
+async function read() {
+	return new Promise(res => {
+		let sum = 0;
+
+		const reader: Interface = createInterface({
+			input: createReadStream('day1/input.txt')
+		});
+
+		reader
+			.on('line', line => {
+				sum = eval(sum + line);
+			})
+			.on('close', () => res(sum));
+	});
+}
+(async function() {
+	console.log(await read());
+})(); // 408
