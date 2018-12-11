195beeb33af01dd3d8dbd38df5ef5caeeff65ff6
Győri Sándor <alexaegis@gmail.com>
2018-12-11 14:36:20 +0100

    Day2 Task1 done

---
+++ day2/task1.ts
@@ -0,0 +1,45 @@
+import { createReadStream } from 'fs';
+import { createInterface } from 'readline';
+
+/**
+ * Returns how many character repeats exactly 'times' times in the given string
+ *
+ * @param line
+ * @param times
+ */
+const charRepeats = (line: string, times: number = 2): number => {
+	let result = 0;
+	const processed: Map<string, number> = new Map();
+	for (let letter of line) {
+		if (!processed.has(letter)) {
+			processed.set(letter, line.split(letter).length - 1);
+		}
+	}
+	return [...processed].filter(([k, v]) => v === times).length;
+};
+
+const atLeastOne = (n: number): boolean => n && n >= 1;
+
+const read = new Promise<number>(async res => {
+	const lineHashes: Array<number> = [];
+	let twiceAppearCount = 0;
+	let thriceAppearCount = 0;
+	const reader = createInterface({
+		input: createReadStream('day2/input.txt')
+	});
+
+	reader.on('line', (line: string) => {
+		twiceAppearCount += atLeastOne(charRepeats(line)) ? 1 : 0;
+		thriceAppearCount += atLeastOne(charRepeats(line, 3)) ? 1 : 0;
+	});
+	reader.on('close', () => {
+		console.log(`File read.`);
+
+		res(twiceAppearCount * thriceAppearCount);
+	});
+});
+
+// IIFEs rule!
+(async function() {
+	console.log(`Resulting checksum: ${await read}`);
+})();
