148c38556e9948cfe7265657b9fb60178d950830
Győri Sándor <alexaegis@gmail.com>
2018-12-16 02:27:03 +0100

    example solved

---
+++ src/2018/day13/part_one.spec.ts
@@ -0,0 +1,7 @@
+import { result } from './part_one';
+
+describe('Day 13, Part One', () => {
+	it('Should be:', async () => {
+		//expect(await result).toEqual('GRTAHKLQVYWXMUBCZPIJFEDNSO');
+	});
+});
