e147ee983f2810a54ca9085d782ddefb479ba884
Győri Sándor <alexaegis@gmail.com>
2018-12-16 03:49:47 +0100

    finished day 13

---
+++ src/2018/day13/part_two.spec.ts
@@ -0,0 +1,12 @@
+import { run } from './part_two';
+
+describe('Day 13, Part Two', () => {
+	it('Should be 6,4 for the example:', async () => {
+		expect(1).toEqual(1);
+		expect((await run('example_two')).toString()).toEqual('6,4');
+	});
+
+	it('Should be 36,123 for the input:', async () => {
+		expect((await run('input')).toString()).toEqual('36,123');
+	});
+});
