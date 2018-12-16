7e692d414651eda79aa44e0bba1756660eb170be
Győri Sándor <alexaegis@gmail.com>
2018-12-16 03:18:43 +0100

    finished part 1

--- src/2018/day13/part_one.spec.ts
+++ src/2018/day13/part_one.spec.ts
@@ -1,7 +1,12 @@
-import { result } from './part_one';
+import { run } from './part_one';
 
 describe('Day 13, Part One', () => {
-	it('Should be:', async () => {
-		//expect(await result).toEqual('GRTAHKLQVYWXMUBCZPIJFEDNSO');
+	it('Should be 7,3 for the example:', async () => {
+		expect(1).toEqual(1);
+		expect((await run('example')).toString()).toEqual('7,3');
+	});
+
+	it('Should be  for the example:', async () => {
+		expect((await run('input')).toString()).toEqual('28,107');
 	});
 });
