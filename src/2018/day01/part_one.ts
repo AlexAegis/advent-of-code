965bd800dcfed77979038e1937b3e8b759c9216f
Győri Sándor <alexaegis@gmail.com>
2019-01-12 14:45:22 +0100

    updated day 1

--- src/2018/day01/part_one.ts
+++ src/2018/day01/part_one.ts
@@ -1,7 +1,12 @@
 import { promises } from 'fs';
 
-export const runner = (async () => {
-	let result = eval(<string>await promises.readFile('src/2018/day01/input.txt', { encoding: 'UTF-8' }));
-	console.log(`Result: ${result}`);
-	return result;
-})(); // 408
+export const runner = async (input: 'example' | 'input' = 'input') =>
+	eval(<string>await promises.readFile(`src/2018/day01/${input}.txt`, { encoding: 'UTF-8' }));
+
+if (require.main == module) {
+	console.time();
+	(async () => {
+		console.log(await runner());
+		console.timeEnd();
+	})(); // 408
+}
