27aed91d40ee5cacbfa84c1f94c4a7f7aa174325
Győri Sándor <alexaegis@gmail.com>
2019-01-12 14:53:51 +0100

    day 2 streamlined

--- src/2018/day02/part_one.spec.ts
+++ src/2018/day02/part_one.spec.ts
@@ -1,7 +1,10 @@
-import { read } from './part_one';
+import { runner } from './part_one';
 
 describe('Day 2, Part One', () => {
-	it('Should be:', async () => {
-		expect(await read).toEqual(5456);
+	it('Should be that the example resolves to 12:', async () => {
+		expect(await runner('example')).toEqual(12);
+	});
+	it('Should be that the input resolves to 5456:', async () => {
+		expect(await runner()).toEqual(5456);
 	});
 });
