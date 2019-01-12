7b75663894fb43213e77e01d4ae0093bc8553003
Győri Sándor <alexaegis@gmail.com>
2019-01-12 14:21:47 +0100

    Day 6 part 2

---
+++ src/2018/day06/coord.class.ts
@@ -0,0 +1,27 @@
+export class Coord {
+	x: number;
+	y: number;
+
+	constructor(x: number, y: number) {
+		this.x = x;
+		this.y = y;
+	}
+
+	add(coord: Coord) {
+		this.x += coord.x;
+		this.y -= coord.y; // Psst, you didn't see me.
+		return this;
+	}
+
+	manhattanCoord(coord: Coord): number {
+		return this.manhattan(coord.x, coord.y);
+	}
+
+	manhattan(x: number, y: number): number {
+		return Math.abs(x - this.x) + Math.abs(y - this.y);
+	}
+
+	toString(): string {
+		return `${this.x},${this.y}`;
+	}
+}
