b4c617d1d49a9beffe6d006bfab9d27e39127a92
Győri Sándor <alexaegis@gmail.com>
2019-01-12 15:06:03 +0100

    day 3 refactor

--- src/2018/day03/part_one.ts
+++ src/2018/day03/part_one.ts
@@ -1,51 +1,35 @@
 import { createReadStream } from 'fs';
 import { createInterface } from 'readline';
+import { interpret } from './interpret.function';
 
-interface Claim {
-	id: number;
-	starting: Coord;
-	size: Coord;
-}
-
-interface Coord {
-	x: number;
-	y: number;
-}
-
-// `${parts[4]}_${parts[5]}`
-const interpret = (line: string): Claim => {
-	let parts = line.split(/#|@|,|:|x/).map(e => e.trim());
-	return {
-		id: Number(parts[1]),
-		starting: { x: Number(parts[2]), y: Number(parts[3]) },
-		size: { x: Number(parts[4]), y: Number(parts[5]) }
-	};
-};
-
-export const read = new Promise<number>(res => {
-	const fabric: Map<string, Array<number>> = new Map<string, Array<number>>(); // Contains each claim for each coordinate
-	const reader = createInterface({
-		input: createReadStream('src/2018/day03/input.txt')
-	});
-	reader
-		.on('line', (line: string) => {
-			let claim = interpret(line);
-			for (let i = claim.starting.x; i < claim.starting.x + claim.size.x; i++) {
-				for (let j = claim.starting.y; j < claim.starting.y + claim.size.y; j++) {
-					const coordKey: string = `${i}_${j}`; // I used string as keys as the coordinates themselves because even if the object looks the same as the key, it's not the exact same key.
-					let claims: Array<number> = [];
-					if (fabric.has(coordKey)) {
-						claims = fabric.get(coordKey);
-					}
-					claims.push(claim.id);
-					fabric.set(coordKey, claims);
-				}
-			}
-		})
-		.on('close', () => {
-			console.log(`File read.`);
-			res([...fabric].filter(([k, v]) => v.length >= 2).length);
+export const runner = async (input: 'example' | 'input' = 'input') =>
+	new Promise<number>(res => {
+		const fabric: Map<string, Array<number>> = new Map<string, Array<number>>(); // Contains each claim for each coordinate
+		const reader = createInterface({
+			input: createReadStream(`src/2018/day03/${input}.txt`)
 		});
-});
+		reader
+			.on('line', (line: string) => {
+				let claim = interpret(line);
+				for (let i = claim.starting.x; i < claim.starting.x + claim.size.x; i++) {
+					for (let j = claim.starting.y; j < claim.starting.y + claim.size.y; j++) {
+						const coordKey: string = `${i}_${j}`; // I used string as keys as the coordinates themselves because even if the object looks the same as the key, it's not the exact same key.
+						let claims: Array<number> = [];
+						if (fabric.has(coordKey)) {
+							claims = fabric.get(coordKey);
+						}
+						claims.push(claim.id);
+						fabric.set(coordKey, claims);
+					}
+				}
+			})
+			.on('close', () => res([...fabric].filter(([k, v]) => v.length >= 2).length));
+	});
 
-(async () => console.log(`Result: ${await read}`))(); // 116920
+if (require.main == module) {
+	console.time();
+	(async () => {
+		console.log(`Resulting checksum: ${await runner()}`);
+		console.timeEnd();
+	})(); // 116920 ~290ms
+}
--- src/2018/day03/part_two.ts
+++ src/2018/day03/part_two.ts
@@ -1,66 +1,54 @@
 import { createReadStream } from 'fs';
 import { createInterface } from 'readline';
+import { Claim } from './claim.interface';
+import { interpret } from './interpret.function';
 
-export interface Claim {
-	id: number;
-	starting: Coord;
-	size: Coord;
-}
-
-export interface Coord {
-	x: number;
-	y: number;
-}
-
-// `${parts[4]}_${parts[5]}`
-const interpret = (line: string): Claim => {
-	let parts = line.split(/#|@|,|:|x/).map(e => e.trim());
-	return {
-		id: Number(parts[1]),
-		starting: { x: Number(parts[2]), y: Number(parts[3]) },
-		size: { x: Number(parts[4]), y: Number(parts[5]) }
-	};
-};
-
-export const read = new Promise<Array<Claim>>(res => {
-	const fabric: Map<string, Array<number>> = new Map<string, Array<number>>(); // Contains each claim for each coordinate
-	const reader = createInterface({
-		input: createReadStream('src/2018/day03/input.txt')
-	});
-	const claims: Array<Claim> = [];
-	reader
-		.on('line', (line: string) => {
-			const claim = interpret(line);
-			claims.push(claim);
-			for (let i = claim.starting.x; i < claim.starting.x + claim.size.x; i++) {
-				for (let j = claim.starting.y; j < claim.starting.y + claim.size.y; j++) {
-					const coordKey: string = `${i}_${j}`; // I used string as keys as the coordinates themselves because even if the object looks the same as the key, it's not the exact same key.
-					let claims: Array<number> = [];
-					if (fabric.has(coordKey)) {
-						claims = fabric.get(coordKey);
-					}
-					claims.push(claim.id);
-					fabric.set(coordKey, claims);
-				}
-			}
-		})
-		.on('close', () => {
-			console.log(`File read.`);
-			const goodClaims: Array<Claim> = [];
-			// Absolutely barbaric
-			for (let claim of claims) {
-				let good = true;
+export const runner = async (input: 'example' | 'input' = 'input') =>
+	new Promise<number>(res => {
+		const fabric: Map<string, Array<number>> = new Map<string, Array<number>>(); // Contains each claim for each coordinate
+		const reader = createInterface({
+			input: createReadStream(`src/2018/day03/${input}.txt`)
+		});
+		const claims: Array<Claim> = [];
+		reader
+			.on('line', (line: string) => {
+				const claim = interpret(line);
+				claims.push(claim);
 				for (let i = claim.starting.x; i < claim.starting.x + claim.size.x; i++) {
 					for (let j = claim.starting.y; j < claim.starting.y + claim.size.y; j++) {
-						good = good && fabric.get(`${i}_${j}`).length === 1;
+						const coordKey: string = `${i}_${j}`;
+						let claims: Array<number> = [];
+						if (fabric.has(coordKey)) {
+							claims = fabric.get(coordKey);
+						}
+						claims.push(claim.id);
+						fabric.set(coordKey, claims);
 					}
 				}
-				if (good) {
-					goodClaims.push(claim);
+			})
+			.on('close', () => {
+				let goodClaim: Claim;
+				// Absolutely barbaric
+				for (let claim of claims) {
+					let good = true;
+					for (let i = claim.starting.x; i < claim.starting.x + claim.size.x; i++) {
+						for (let j = claim.starting.y; j < claim.starting.y + claim.size.y; j++) {
+							good = good && fabric.get(`${i}_${j}`).length === 1;
+						}
+					}
+					if (good) {
+						goodClaim = claim;
+						break;
+					}
 				}
-			}
-			res(goodClaims);
-		});
-});
+				res(goodClaim.id);
+			});
+	});
 
-(async () => console.log(`Result: ${await read}`))(); // [{"id":382,"starting":{"x":155,"y":316},"size":{"x":28,"y":15}}]
+if (require.main == module) {
+	console.time();
+	(async () => {
+		console.log(`Resulting checksum: ${await runner()}`);
+		console.timeEnd();
+	})(); // 382 ~240ms
+}
