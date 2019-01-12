60dd8a9c5408169b803b3a1b525db9c9b2d45b33
Győri Sándor <alexaegis@gmail.com>
2019-01-12 15:24:02 +0100

    day 5 refactored

--- src/2018/day05/part_one.spec.ts
+++ src/2018/day05/part_one.spec.ts
@@ -2,6 +2,10 @@ import { runner } from './part_one';
 
 describe('Day 5, Part One', () => {
 	it('Should be:', async () => {
-		expect(await runner).toEqual(9202);
+		expect(await runner('example')).toEqual(10);
+	});
+
+	it('Should be:', async () => {
+		expect(await runner()).toEqual(9202);
 	});
 });
