77f8294c937f1a249f9284d36e325dd71783232f
Győri Sándor <alexaegis@gmail.com>
2019-01-12 15:24:02 +0100

    day 5 refactored

--- src/2018/day05/part_one.ts
+++ src/2018/day05/part_one.ts
@@ -1,21 +1,14 @@
 import * as fs from 'fs';
+import { collapse } from './collapse.function';
 
-/**
- * For the uncompressed solution check the previous commit
- *
- * @param sequence to be collapsed
- */
-const collapse = (sequence: string) =>
-	[...sequence].reduce((acc, curr) =>
-		acc && acc.charAt(acc.length - 1) !== curr && acc.charAt(acc.length - 1).toLowerCase() === curr.toLowerCase()
-			? acc.substr(0, acc.length - 1)
-			: acc + curr
-	);
+export const runner = async (input: 'example' | 'input' = 'input') => {
+	return collapse(<string>await fs.promises.readFile(`src/2018/day05/${input}.txt`, { encoding: 'UTF-8' })).length;
+};
 
-export const runner = (async () => {
-	let collapsedSequence = collapse(<string>(
-		await fs.promises.readFile('src/2018/day05/input.txt', { encoding: 'UTF-8' })
-	)); // Encoding is specified, result is string
-	console.log(`Collapsed sequences length: ${collapsedSequence.length}`); // 9202
-	return collapsedSequence.length;
-})();
+if (require.main == module) {
+	console.time();
+	(async () => {
+		console.log(`Collapsed sequences length: ${await runner('example')}`);
+		console.timeEnd();
+	})(); // 9202 ~26ms
+}
--- src/2018/day05/part_two.ts
+++ src/2018/day05/part_two.ts
@@ -1,27 +1,12 @@
 import * as fs from 'fs';
+import { collapse } from './collapse.function';
 
-/**
- * For the uncompressed solution check the previous commit
- *
- * @param sequence to be collapsed
- */
-const collapse = (sequence: string) =>
-	[...sequence].reduce((acc, curr) =>
-		acc && acc.charAt(acc.length - 1) !== curr && acc.charAt(acc.length - 1).toLowerCase() === curr.toLowerCase()
-			? acc.substr(0, acc.length - 1)
-			: acc + curr
-	);
+export const runner = async (input: 'example' | 'input' = 'input') => {
+	let sequence = <string>await fs.promises.readFile(`src/2018/day05/${input}.txt`, { encoding: 'UTF-8' }); // Encoding is specified, result is string
 
-export const runner = (async () => {
-	console.time();
-	let sequence = <string>await fs.promises.readFile('src/2018/day05/input.txt', { encoding: 'UTF-8' }); // Encoding is specified, result is string
-
-	// get all unique letters regardless of casing
 	const uniqueUnits = [...sequence].reduce((acc, curr) =>
 		acc.includes(curr.toLowerCase()) ? acc.toLowerCase() : acc + curr.toLowerCase()
 	);
-
-	console.log(uniqueUnits);
 
 	let shortestSequence: string;
 	let shortestSequenceRemovedUnit: string;
@@ -29,18 +14,20 @@ export const runner = (async () => {
 	for (let unit of uniqueUnits) {
 		let modifiedSequence = [...sequence].reduce((acc, curr) => (curr.toLowerCase() === unit ? acc : acc + curr));
 		let collapsedSequence = collapse(modifiedSequence);
-		console.log(
-			`Remaining sequences length: ${collapsedSequence.length}, we used a sequence without the ${unit} unit.`
-		);
-
 		if (shortestSequence === undefined || collapsedSequence.length < shortestSequence.length) {
 			shortestSequence = collapsedSequence;
 			shortestSequenceRemovedUnit = unit;
 		}
 	}
 
-	console.log(
-		`Shortest sequence is ${shortestSequence.length} long. The removed unit is: ${shortestSequenceRemovedUnit}`
-	); // 6394, with the removal of k and K
+	console.log(`The removed unit is: ${shortestSequenceRemovedUnit}`);
 	return shortestSequence.length;
-})();
+};
+
+if (require.main == module) {
+	console.time();
+	(async () => {
+		console.log(`Shortest sequence is: ${await runner()}`);
+		console.timeEnd();
+	})(); // 6394 ~361ms
+}
