1ecd6e4c5f60c85938794a1a3b2ef37588c0b100
Győri Sándor <alexaegis@gmail.com>
2019-01-12 02:51:21 +0100

    hmm

--- src/2018/day06/part_one.spec.ts
+++ src/2018/day06/part_one.spec.ts
@@ -1,4 +1,4 @@
-import { Coord } from './part_one';
+import { runner, Coord } from './part_one';
 
 describe('Day 6, Part One', () => {
 	const a: Coord = new Coord(1, 1);
@@ -6,15 +6,23 @@ describe('Day 6, Part One', () => {
 	const c: Coord = new Coord(7, 2);
 
 	it('should the manhattan distance between (1, 1) and (3, 3) be 4', async () => {
-		expect(a.manhattan(b)).toEqual(4);
+		expect(a.manhattanCoord(b)).toEqual(4);
 	});
 
 	it('should the manhattan distance between (3, 3) and (7, 2) be 4', async () => {
-		expect(b.manhattan(c)).toEqual(5);
+		expect(b.manhattanCoord(c)).toEqual(5);
 	});
 
 	it('should the that the manhattan distance is commutative', async () => {
-		expect(a.manhattan(b)).toEqual(b.manhattan(a));
-		expect(a.manhattan(c)).toEqual(c.manhattan(a));
+		expect(a.manhattanCoord(b)).toEqual(b.manhattanCoord(a));
+		expect(a.manhattanCoord(c)).toEqual(c.manhattanCoord(a));
+	});
+
+	it('should the example resolve to 17', async () => {
+		expect(await runner('example')).toEqual(17);
+	});
+
+	it('should the input resolve to XX', async () => {
+		expect(await runner('input')).toEqual(3006);
 	});
 });
--- src/2018/day06/part_one.ts
+++ src/2018/day06/part_one.ts
@@ -18,8 +18,12 @@ export class Coord {
 		return this;
 	}
 
-	manhattan(coord: Coord) {
-		return Math.abs(coord.x - this.x) + Math.abs(coord.y - this.y);
+	manhattanCoord(coord: Coord): number {
+		return this.manhattan(coord.x, coord.y);
+	}
+
+	manhattan(x: number, y: number): number {
+		return Math.abs(x - this.x) + Math.abs(y - this.y);
 	}
 
 	toString(): string {
@@ -35,16 +39,95 @@ export const read = (input: 'example' | 'input' = 'example'): Promise<Array<Coor
 		})
 			.on('line', line => {
 				const lineSplit = line.split(', ');
-				points.push(new Coord(Number(lineSplit[0]), Number(lineSplit[1])));
+				points.push(new Coord(Number(lineSplit[1]), Number(lineSplit[0])));
 			})
 			.on('close', () => res(points));
 	});
 
-export const autorun = (async (input: 'example' | 'input' = 'example'): Promise<any> =>
+export const runner = async (input: 'example' | 'input' = 'example'): Promise<any> =>
 	new Promise<any>(async res => {
 		console.time();
 		const points = await read(input);
 		console.log(points);
 
+		/*	PLAN- BOUNDING BOX
+
+			get the bounding box of all the points and pad it by 1.
+			start iterating from the center in circles until the bounding box is complete
+			while iterating, for each point get the manhattan distance for each point, get the lowest ones 
+			if there is only one, then put that point in the area of that point, if there are multiple ones, ignore it, its a boundary. 
+
+		*/
+
+		let boundaryTop: Coord;
+		let boundaryRight: Coord;
+		let boundaryBottom: Coord;
+		let boundaryLeft: Coord;
+
+		points.forEach(point => {
+			if (boundaryTop === undefined || boundaryTop.y >= point.y) {
+				boundaryTop = point;
+			}
+			if (boundaryRight === undefined || boundaryRight.x <= point.x) {
+				boundaryRight = point;
+			}
+			if (boundaryBottom === undefined || boundaryBottom.y <= point.y) {
+				boundaryBottom = point;
+			}
+			if (boundaryLeft === undefined || boundaryLeft.x >= point.x) {
+				boundaryLeft = point;
+			}
+		});
+
+		const boundaryStart: Coord = new Coord(boundaryLeft.x - 10, boundaryTop.y - 10);
+		const boundaryEnd: Coord = new Coord(boundaryRight.x + 20, boundaryBottom.y + 20);
+		// 40:52 - 353:358
+		const bucket: Map<string, Array<Coord>> = new Map();
+		const associations: Map<string, string> = new Map();
+		let i = 0;
+		for (let point of points) {
+			bucket.set(point.toString(), []);
+			associations.set(point.toString(), String.fromCharCode(64 + i)); // 97
+			i++;
+		}
+		let map: Array<string> = [];
+		for (let x = boundaryStart.x; x < boundaryEnd.x; x++) {
+			let row = '';
+			for (let y = boundaryStart.y; y < boundaryEnd.y; y++) {
+				let ordered: Array<Coord> = points.sort((a, b) => a.manhattan(x, y) - b.manhattan(x, y));
+				if (ordered[0].manhattan(x, y) !== ordered[1].manhattan(x, y)) {
+					bucket.get(ordered[0].toString()).push(new Coord(x, y));
+					let assoc = associations.get(ordered[0].toString());
+					if (ordered[0].toString() === new Coord(x, y).toString()) {
+						assoc = assoc.toUpperCase();
+					}
+					row = row + assoc;
+				} else {
+					row = row + '.';
+				}
+			}
+			map.push(row);
+		}
+		//map.forEach(row => console.log(row));
+		const bound: Array<number> = [];
+		bucket.forEach(territory => {
+			if (
+				!territory.some(
+					point =>
+						point.x <= boundaryStart.x + 10 ||
+						point.y <= boundaryStart.y + 20 ||
+						point.x >= boundaryEnd.x - 20 ||
+						point.y >= boundaryEnd.y - 20
+				)
+			) {
+				bound.push(territory.length);
+			}
+		});
+		let sortedBound = bound.sort((a, b) => a - b);
+
+		const max = bound.reduce((acc, next) => (acc === undefined || next > acc ? next : acc));
+		res(max);
 		console.timeEnd();
-	}))();
+	}); // 3006 the partitioning of the plane is good, but the way I determine which is infinite or not is not.
+
+(async () => console.log(await runner('input')))();
