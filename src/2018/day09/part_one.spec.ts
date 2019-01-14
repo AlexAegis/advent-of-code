ce1f974a8a046830e51404e881c681193e8330d4
Győri Sándor <alexaegis@gmail.com>
2019-01-14 23:45:45 +0100

    added examples

--- src/2018/day09/part_one.spec.ts
+++ src/2018/day09/part_one.spec.ts
@@ -2,9 +2,30 @@ import { runner } from './part_one';
 
 describe('Day 9, Part One', () => {
 	it('Should be:', async () => {
-		expect(await runner('example')).toEqual(138);
+		expect(await runner('example_0')).toEqual(32);
 	});
+
 	it('Should be:', async () => {
-		expect(await runner()).toEqual(47112);
+		expect(await runner('example_1')).toEqual(8317);
+	});
+
+	it('Should be:', async () => {
+		expect(await runner('example_2')).toEqual(146373);
+	});
+
+	it('Should be:', async () => {
+		expect(await runner('example_3')).toEqual(2764);
+	});
+
+	it('Should be:', async () => {
+		expect(await runner('example_4')).toEqual(54718);
+	});
+
+	it('Should be:', async () => {
+		expect(await runner('example_5')).toEqual(37305);
+	});
+
+	it('Should be:', async () => {
+		expect(await runner()).toEqual(0);
 	});
 });
--- src/2018/day09/part_two.spec.ts
+++ src/2018/day09/part_two.spec.ts
@@ -1,10 +1,10 @@
 import { runner } from './part_two';
 
-describe('Day 9, Part One', () => {
+describe('Day 9, Part Two', () => {
 	it('Should be:', async () => {
 		expect(await runner('example')).toEqual(66);
 	});
 	it('Should be:', async () => {
-		expect(await runner()).toEqual(28237);
+		expect(await runner()).toEqual(0);
 	});
 });
