9640d9e8f04616d084229a3ec90e54726057482f
Győri Sándor <alexaegis@gmail.com>
2019-01-14 23:51:10 +0100

    Day 9 Part 1

--- src/2018/day09/part_one.ts
+++ src/2018/day09/part_one.ts
@@ -1,6 +1,4 @@
 import { reader } from './reader.function';
-import { interval } from 'rxjs';
-import { take } from 'rxjs/operators';
 
 export const runner = async (input: string = 'input'): Promise<any> =>
 	new Promise<any>(async res => {
@@ -10,33 +8,30 @@ export const runner = async (input: string = 'input'): Promise<any> =>
 		let current: number = 0;
 		let score: Map<number, number> = new Map();
 
-		interval()
-			.pipe(take(setup.lastMarble + 1))
-			.subscribe(marble => {
-				if (marble !== 0 && marble % 23 === 0) {
-					const currentPlayer = marble % setup.players;
-					if (!score.has(currentPlayer)) {
-						score.set(currentPlayer, 0);
-					}
-					score.set(currentPlayer, score.get(currentPlayer) + marble);
-					current = current - 7;
-					if (current < 0) {
-						current = ring.length + current;
-					}
-					score.set(currentPlayer, score.get(currentPlayer) + ring.splice(current - 1, 1)[0]);
-				} else {
-					ring.splice(current + 1, 0, marble);
-					current = (current + 2) % ring.length;
+		for (let marble = 0; marble <= setup.lastMarble; marble++) {
+			if (marble !== 0 && marble % 23 === 0) {
+				const currentPlayer = marble % setup.players;
+				if (!score.has(currentPlayer)) {
+					score.set(currentPlayer, 0);
 				}
-				// console.log(`ring:  ${JSON.stringify(ring)}`);
-			})
-			.add(() => res([...score].reduce((acc, next) => (acc[1] < next[1] ? next : acc))[1]));
+				score.set(currentPlayer, score.get(currentPlayer) + marble);
+				current = current - 7;
+				if (current < 0) {
+					current = ring.length + current;
+				}
+				score.set(currentPlayer, score.get(currentPlayer) + ring.splice(current - 1, 1)[0]);
+			} else {
+				ring.splice(current + 1, 0, marble);
+				current = (current + 2) % ring.length;
+			}
+		}
+		res([...score].reduce((acc, next) => (acc[1] < next[1] ? next : acc))[1]);
 	});
 
 if (require.main == module) {
 	console.time();
 	(async () => {
-		console.log(`${await runner('example_2')}`);
+		console.log(`${await runner()}`);
 		console.timeEnd();
-	})(); //  ~ms
+	})(); // 361466 ~140ms
 }
