17a80ecd71c58716a4aa36f05b9f479ced37be25
Győri Sándor <alexaegis@gmail.com>
2019-01-15 08:39:13 +0100

    jesus

--- src/2018/day09/part_two.ts
+++ src/2018/day09/part_two.ts
@@ -1,12 +1,37 @@
 import { reader } from './reader.function';
 
-export const runner = async (input: 'example' | 'input' = 'input'): Promise<any> =>
-	new Promise<any>(async res => res(await reader(input)));
+export const runner = async (input: string = 'input'): Promise<any> =>
+	new Promise<any>(async res => {
+		const setup = await reader(input);
+
+		let ring: Array<number> = [];
+		let current: number = 0;
+		let score: Map<number, number> = new Map();
+
+		for (let marble = 0; marble <= setup.lastMarble * 100; marble++) {
+			if (marble !== 0 && marble % 23 === 0) {
+				const currentPlayer = marble % setup.players;
+				if (!score.has(currentPlayer)) {
+					score.set(currentPlayer, 0);
+				}
+				score.set(currentPlayer, score.get(currentPlayer) + marble);
+				current = current - 7;
+				if (current < 0) {
+					current = ring.length + current;
+				}
+				score.set(currentPlayer, score.get(currentPlayer) + ring.splice(current - 1, 1)[0]);
+			} else {
+				ring.splice(current + 1, 0, marble);
+				current = (current + 2) % ring.length;
+			}
+		}
+		res([...score].reduce((acc, next) => (acc[1] < next[1] ? next : acc))[1]);
+	});
 
 if (require.main == module) {
 	console.time();
 	(async () => {
 		console.log(`${await runner()}`);
 		console.timeEnd();
-	})(); // ~ms
+	})(); // 10 times: 30250091 ~21216.732ms 100 times: 2945918550 ~5203622.781ms
 }
