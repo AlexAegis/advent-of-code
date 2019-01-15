eed1aa42dd25cce29184be640e994ba69005f96a
Győri Sándor <alexaegis@gmail.com>
2019-01-15 10:57:46 +0100

    input reading

--- src/2018/day10/part_one.ts
+++ src/2018/day10/part_one.ts
@@ -4,6 +4,7 @@ export const runner = async (input: string = 'input'): Promise<any> =>
 	new Promise<any>(async res => {
 		const vectors = await reader(input);
 		//console.log(JSON.stringify(vectors));
+
 		res(0);
 	});
 
