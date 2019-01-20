e738fa56d97ef229d6e76b4326d08467f63eb2c2
Győri Sándor <alexaegis@gmail.com>
2019-01-20 04:19:15 +0100

    enabled test as deque is introduced

--- src/2018/day09/part_two.spec.ts
+++ src/2018/day09/part_two.spec.ts
@@ -2,6 +2,6 @@ import { runner } from './part_two';
 
 describe('Day 9, Part Two', () => {
 	it('Should be:', async () => {
-		expect(2945918550 /*await runner()*/).toEqual(2945918550);
+		expect(await runner()).toEqual(2945918550);
 	});
 });
