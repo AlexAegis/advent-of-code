50ca16a07954b076b1c7172fd13b081233d2ca62
Győri Sándor <alexaegis@gmail.com>
2019-02-16 04:34:46 +0100

    slow solution

--- src/2018/day11/part_two.rework.ts
+++ src/2018/day11/part_two.rework.ts
@@ -13,27 +13,42 @@ export const runner = async (input: number = 8561): Promise<string> =>
 			promises.push(
 				pool.exec(
 					(cx: number, cy: number, mapSize: number, input: number) => {
+						let ripple = (
+							radius: number,
+							xoffset: number = 0,
+							yoffset: number = 0
+						): Array<{ x: number; y: number }> => {
+							const result = [];
+							if (radius === 0) {
+								result.push({ x: xoffset, y: yoffset });
+							}
+							for (let i = -radius; i < radius; i++) {
+								result.push({ x: -radius + xoffset, y: 1 + i + yoffset });
+								result.push({ x: radius + xoffset, y: i + yoffset });
+								result.push({ x: i + xoffset, y: -radius + yoffset });
+								result.push({ x: 1 + i + xoffset, y: radius + yoffset });
+							}
+							return result;
+						};
+
 						let calc = (x: number, y: number) => {
 							let rackID = x + 10;
-							let power = rackID * y;
-							power += input;
-							power *= rackID;
-							power = Math.floor((power % 1000) / 100);
-							power -= 5;
-							return power;
+							let powerLevel = rackID * y;
+							powerLevel += input;
+							powerLevel *= rackID;
+							powerLevel = Math.floor((powerLevel % 1000) / 100);
+							powerLevel -= 5;
+							return powerLevel;
 						};
 						let maxSize = Math.min(cx, cy, mapSize - cx, mapSize - cy);
 						let acc = { coord: { x: cx, y: cy }, sum: -Infinity, size: -Infinity };
-						for (let size = 0; size < maxSize; size++) {
-							let lol: Array<{ x: number; y: number }> = [];
-							for (let x = cx; x <= cx + size; x++) {
-								for (let y = cy; y <= cy + size; y++) {
-									lol.push({ x: x, y: y });
-								}
-							}
-							let sum = lol.map(c => calc(c.x, c.y)).reduce((acc, next) => (acc += next), 0);
+						let sum = 0;
+						for (let ring = 0; ring < 7; ring++) {
+							sum = ripple(ring, cx, cy)
+								.map(c => calc(c.x, c.y))
+								.reduce((acc, next) => (acc += next), sum);
 							if (sum > acc.sum) {
-								acc.size = size + 1;
+								acc.size = ring * 2;
 								acc.sum = sum;
 							}
 						}
@@ -66,4 +81,5 @@ if (require.main == module) {
 		console.log(`Result: ${await runner()}`);
 		console.timeEnd();
 	})(); // 236,146,12 (160) ~63007ms on i7-8700k@4.8Ghz all 6 cores 100%
+	// err 242,150,7 (154) ~207273.412ms with limit and without too
 }
