b53f1686d3a1615d20305ca0cc6808064e893382
Győri Sándor <alexaegis@gmail.com>
2019-02-17 13:31:43 +0100

    Day 14 Part 2

--- src/2018/day14/part_two.spec.ts
+++ src/2018/day14/part_two.spec.ts
@@ -2,7 +2,31 @@ import { runner } from './part_two';
 import { expect } from 'chai';
 
 describe('Day 14, Part Two', () => {
-	it('Should be 5158916779 after 9 recipe:', async () => {
-		expect((await runner(9)).toString()).to.equal('5158916779');
+	it('Should take 9 recipes for 5158916779 to appear:', async () => {
+		expect((await runner('5158916779')).toString()).to.equal(9);
+	});
+	it('Should take 9 recipes for 51589 to appear:', async () => {
+		expect((await runner('51589')).toString()).to.equal(9);
+	});
+	it('Should take 5 recipes for 0124515891 to appear:', async () => {
+		expect((await runner('0124515891')).toString()).to.equal(5);
+	});
+	it('Should take 5 recipes for 01245 to appear:', async () => {
+		expect((await runner('01245')).toString()).to.equal(5);
+	});
+	it('Should take 18 recipes for 9251071085 to appear:', async () => {
+		expect((await runner('9251071085')).toString()).to.equal(18);
+	});
+	it('Should take 18 recipes for 92510 to appear:', async () => {
+		expect((await runner('92510')).toString()).to.equal(18);
+	});
+	it('Should take 2018 recipes for 5941429882 to appear:', async () => {
+		expect((await runner('5941429882')).toString()).to.equal(2018);
+	});
+	it('Should take 2018 recipes for 59414 to appear:', async () => {
+		expect((await runner('59414')).toString()).to.equal(2018);
+	});
+	it('Should take 20229822 recipes for 327901 to appear:', async () => {
+		expect((await runner()).toString()).to.equal(20229822);
 	});
 });
--- src/2018/day14/part_two.ts
+++ src/2018/day14/part_two.ts
@@ -1,20 +1,33 @@
-export const runner = async (input: number = 327901): Promise<any> =>
+function shift(window: string, last: number, length: number): string {
+	if (window.length >= length) {
+		return window.slice(window.length - length + 1, window.length) + last;
+	} else return window + last;
+}
+
+export const runner = async (input: string = '327901'): Promise<any> =>
 	new Promise<any>(async res => {
 		const recipes: Array<number> = [3, 7];
 		let a = 0;
 		let b = 1;
-		for (let i = 0; recipes.length <= input + 10; i++) {
+		let window: string = '';
+		while (true) {
 			const next = recipes[a] + recipes[b];
 			if (next >= 10) {
 				recipes.push(Math.floor(next / 10));
+				window = shift(window, recipes[recipes.length - 1], input.length);
+				if (window === input) break;
 				recipes.push(next % 10);
+				window = shift(window, recipes[recipes.length - 1], input.length);
+				if (window === input) break;
 			} else {
 				recipes.push(next);
+				window = shift(window, recipes[recipes.length - 1], input.length);
+				if (window === input) break;
 			}
 			a = (a + recipes[a] + 1) % recipes.length;
 			b = (b + recipes[b] + 1) % recipes.length;
 		}
-		res(recipes.splice(input, 10).join(''));
+		res(recipes.length - input.length);
 	});
 
 if (require.main == module) {
@@ -22,5 +35,5 @@ if (require.main == module) {
 	(async () => {
 		console.log(`${await runner()}`);
 		console.timeEnd();
-	})(); //
+	})(); // 20229822 ~ 1024ms
 }
