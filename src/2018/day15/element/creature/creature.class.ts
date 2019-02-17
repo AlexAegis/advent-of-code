8889d02b4b3e03a0f0c19a5ff0127dc907413ffe
Győri Sándor <alexaegis@gmail.com>
2019-02-17 20:24:49 +0100

    two way binding

--- src/2018/day15/element/creature/creature.class.ts
+++ src/2018/day15/element/creature/creature.class.ts
@@ -1,6 +1,18 @@
 import { Element } from '../element.class';
+import { Ground } from '../block/ground.class';
 
 export class Creature extends Element {
 	hp: number = 200;
 	ap: number = 3;
+
+	_ground: Ground;
+
+	set ground(ground: Ground) {
+		this._ground = ground;
+		ground._occupant = this;
+	}
+
+	get ground(): Ground {
+		return this._ground;
+	}
 }
