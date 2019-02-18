118ae55f45be61b5a8ce15efc39b1cce8239db28
Győri Sándor <alexaegis@gmail.com>
2019-02-19 00:50:46 +0100

    typings

--- src/2018/day15/test.ts
+++ src/2018/day15/test.ts
@@ -2,11 +2,26 @@ import { elementFactory } from './element/element.factory';
 import { Goblin } from './element/creature/goblin.class';
 import { Wall } from './element/block/wall.class';
 import { AVL } from './path/avl.class';
+import { Convertable } from './path/convertable.interface';
 
-const tree: AVL.Tree<number> = new AVL.Tree<number>();
+const tree: AVL.Tree<number, Macska> = new AVL.Tree<number, number>();
 
-tree.insert(-6, -4, -1, 1, 2, 3, 4, 5, 6, 11, 12);
-/*tree.insert(2);
+export class Kutya implements Convertable<number> {
+	n: number = 0;
+	constructor(n: number) {
+		//super();
+		this.n = n;
+	}
+	convertTo(): number {
+		return this.n;
+	}
+}
+
+export class Macska {}
+//tree.insert(-6, -4, -1, 1, 2, 3, 4, 5, 6, 11, 12);
+/*
+tree.insert(1);
+tree.insert(2);
 tree.insert(3);
 tree.insert(4);
 tree.insert(5);
@@ -16,6 +31,22 @@ tree.insert(-1);
 tree.insert(12);
 tree.insert(-4);
 tree.insert(11);*/
+
+tree.push(1);
+tree.push(2);
+tree.push(3);
+tree.push(4);
+tree.push(5);
+tree.push(6);
+tree.push(7);
+tree.set(3, 4);
+
+let kutyus = new Kutya(10);
+
+let cica = new Macska();
+tree.push(kutyus);
+tree.set(2, cica);
+/**/
 
 // 11 elments
 
