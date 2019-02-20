e00621f85d4762744b27269210ed517815477bdc
Győri Sándor <alexaegis@gmail.com>
2019-02-20 09:16:39 +0100

    faster d1p2 solution

--- src/2018/day01/part_two.bench.ts
+++ src/2018/day01/part_two.bench.ts
@@ -4,7 +4,10 @@ import { Tree } from '@alexaegis/avl';
 import { SimpleTree } from '@alexaegis/avl';
 
 export const runner = async (input: 'example' | 'input' = 'input'): Promise<number> => {
+	//let frequencyHistory = new Tree<number>();
+	//let frequencyHistory = new SimpleTree();
 	let frequencyHistory = new Set();
+	//let frequencyHistory: Array<number> = [];
 	let cache: Array<number> = [];
 	let fileRead: boolean = false;
 	let sumTotal: number = 0;
@@ -14,19 +17,54 @@ export const runner = async (input: 'example' | 'input' = 'input'): Promise<numb
 	const reader = createInterface({
 		input: createReadStream(`src/2018/day01/${input}.txt`)
 	});
+	// frequencyHistory.push(0);
 	const calculate = (line: number) => {
 		if (!fileRead) {
 			cache.push(line);
 		}
 		sumTotal += line;
 		if (!firstToBeTwice) {
+			//console.time();
+
+			//if (frequencyHistory.has(sumTotal)) {
+			/*if (frequencyHistory.find(val => val === sumTotal)) {
+				firstToBeTwice = sumTotal;
+			}*/
 			let prevLength = frequencyHistory.size;
 			frequencyHistory.add(sumTotal);
 			if (frequencyHistory.size === prevLength) {
 				firstToBeTwice = sumTotal;
 			}
+			//console.timeEnd();
 		}
 	};
+
+	/*
+	tree
+	whole: 
+whole: 0.157958984375ms
+whole: 0.285ms
+
+
+access: 0.195ms
+access: 0.009033203125ms
+
+push: 0.067138671875ms
+push: 0.186ms
+
+simpletree:
+whole: 0.31103515625ms
+whole: 0.466ms
+
+array:
+whole: 0.027099609375ms
+whole: 0.141ms
+
+access: 0.011962890625ms
+access: 0.264ms
+push: 0.004150390625ms
+push: 0.203ms
+	*/
 
 	const prom = new Promise<number>(res => {
 		reader
@@ -39,9 +77,16 @@ export const runner = async (input: 'example' | 'input' = 'input'): Promise<numb
 	});
 	await prom;
 
+	let iter = 0;
 	while (firstToBeTwice === undefined) {
-		cache.forEach(calculate);
+		//for (let i = 0; i < 129465; i++) {
+		for (let o of cache) {
+			calculate(o);
+			iter++;
+		}
+		//cache.forEach(calculate);
 	}
+	console.log(`took i: ${iter} iterations to find`); // 129465
 
 	return firstToBeTwice;
 };
@@ -51,5 +96,5 @@ if (require.main === module) {
 	(async () => {
 		console.log(`First to be repeated: ${await runner()}`);
 		console.timeEnd();
-	})(); // 55250 ~19ms
+	})(); // 55250 ~22ms
 }
--- src/2018/day01/part_two.spec.ts
+++ src/2018/day01/part_two.spec.ts
@@ -3,10 +3,7 @@ import { expect } from 'chai';
 
 describe('Day 1, Part Two', () => {
 	it('Should be:', async function() {
-		// Lambdas bind 'this' lexically, and thus the mocha context is not usable.
-		this.timeout(10000);
-		// await runner(); // it takes 5000ms so I won't run it every time.
-		expect(55250).to.equal(55250);
+		expect(await runner()).to.equal(55250);
 	});
 
 	it('Should be that the example resolves to :', async () => {
