89e23c036853e4c650cb5b13358f36db491fbebc
Győri Sándor <alexaegis@gmail.com>
2019-03-28 16:47:47 +0100

    updated packages

--- src/2018/day15/part_one.ts
+++ src/2018/day15/part_one.ts
@@ -1,21 +1,29 @@
 import { reader } from './reader.function';
 import { Cave } from './cave.class';
-import * as pf from 'ngraph.path';
 
 export const runner = async (input: string = 'input'): Promise<any> =>
 	new Promise<any>(async res => {
 		const cave: Cave = await reader(input);
 		for (let unit of cave.units()) {
 			console.log(unit);
-			pf.nba([], {});
 		}
 		res(cave);
 	});
-
+/*
 if (require.main == module) {
 	console.time();
 	(async () => {
 		console.log(`${await runner('example.1')}`);
 		console.timeEnd();
 	})(); //
-}
+}*/
+
+console.log(typeof {});
+
+let n = 10;
+
+let asd = () => {
+	console.log(this.n);
+};
+let a = { n: 10 };
+asd();
