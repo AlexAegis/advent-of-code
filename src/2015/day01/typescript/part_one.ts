d19e950e0bee461ca4a290bfa1108778fbb8dee0
Győri Sándor <alexaegis@gmail.com>
2019-06-01 03:00:20 +0200

    rust example, better js benchmark prototype

--- src/2015/day01/typescript/part_one.ts
+++ src/2015/day01/typescript/part_one.ts
@@ -1,12 +1,9 @@
 import { reader } from './reader.function';
+import { bench } from '@root/bench.function';
 
 export const runner = async (input: string) =>
 	[...input].filter(c => c === '(' || c === ')').reduce((a, n) => a + (n === '(' ? 1 : -1), 0);
 
 if (require.main === module) {
-	console.time();
-	(async () => {
-		console.log(await runner(await reader()));
-		console.timeEnd();
-	})(); // 74 ~8ms
+	(async () => console.log(`Result: ${await bench(reader, runner)}`))(); // 74 ~0.5ms
 }
---
+++ src/bench.function.ts
@@ -0,0 +1,32 @@
+import { performance, PerformanceObserver } from 'perf_hooks';
+
+/**
+ * Wrapper for running read/compute pairs. Benchmarks both using the performance api.
+ *
+ * @param reader Async input supplier for the `runner`
+ * @param runner with the result of `reader`, produces an output
+ */
+export const bench = async <T>(reader: () => Promise<T>, runner: (input: T) => Promise<any>) => {
+	performance.mark('start');
+	const obs = new PerformanceObserver(list => {
+		list.getEntries().forEach(entry => {
+			console.log(`${entry.name}: ${entry.duration}`);
+		});
+	});
+	obs.observe({ entryTypes: ['measure'] });
+
+	performance.mark('readstart');
+	const input = await reader();
+	performance.mark('readend');
+
+	performance.mark('runstart');
+	const result = await runner(input);
+	performance.mark('runend');
+
+	performance.measure('run', 'runstart', 'runend');
+	performance.measure('read', 'readstart', 'readend');
+	performance.measure('both', 'readstart', 'runend');
+	performance.measure('total', 'start', 'runend');
+	obs.disconnect();
+	return result;
+};
