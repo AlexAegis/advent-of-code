7dd48ac85d8b55d3137a97e59f2e53aa50e4b0da
Győri Sándor <alexaegis@gmail.com>
2019-06-01 22:31:01 +0200

    added rust example

---
+++ src/2015/day01/rust/tests/tests.rs
@@ -0,0 +1,7 @@
+extern crate aoc150101;
+
+#[test]
+fn it_should_return_74_for_input() {
+	assert_eq!(aoc150101::runner("))".to_string()), 0);
+	assert_eq!(aoc150101::runner("))".to_string()), 0);
+}
