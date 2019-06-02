1d971b5c5411111f885581ca6aef5872fa24d39f
Győri Sándor <alexaegis@gmail.com>
2019-06-02 18:22:07 +0200

    removed rust coverage prototyping

--- script/merge_lcov.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Credits: https://gist.github.com/xseignard/5420661
-
-while read FILENAME; do
-    LCOV_INPUT_FILES="$LCOV_INPUT_FILES -a \"$FILENAME\""
-done < <( find $1 -name lcov.info )
-
-eval lcov "${LCOV_INPUT_FILES}" -o $1/$2
