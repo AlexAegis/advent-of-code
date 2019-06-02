c56e8d7fbb2520331e8d14f91d11022e40fa7b72
Győri Sándor <alexaegis@gmail.com>
2019-06-02 14:04:37 +0200

    fixed mocha tsconfig register

--- src/2015/day01/typescript/part_one.ts
+++ src/2015/day01/typescript/part_one.ts
@@ -1,9 +1,9 @@
-import { reader } from './reader.function';
+import { reader } from '@root/reader.function';
 import { bench } from '@root/bench.function';
 
 export const runner = async (input: string) =>
 	[...input].filter(c => c === '(' || c === ')').reduce((a, n) => a + (n === '(' ? 1 : -1), 0);
 
 if (require.main === module) {
-	(async () => console.log(`Result: ${await bench(reader, runner)}`))(); // 74 ~0.5ms
+	(async () => console.log(`Result: ${await bench(reader(2015, 1, 'input.txt'), runner)}`))(); // 74 ~0.5ms
 }
