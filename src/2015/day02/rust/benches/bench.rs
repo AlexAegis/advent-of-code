4eb7015a9dc2daf861bf27e9a31338f79ca6acd9
Győri Sándor <alexaegis@gmail.com>
2019-06-09 21:33:22 +0200

    rust solution for 2015 day 2

---
+++ src/2015/day02/rust/benches/bench.rs
@@ -0,0 +1,26 @@
+#[macro_use]
+
+extern crate criterion;
+
+extern crate aoc;
+extern crate aoc1502;
+
+use aoc::Solvable;
+use criterion::Criterion;
+
+fn part_one_benchmark(c: &mut Criterion) {
+	c.bench_function("2015 day 2 part one", |b| {
+		let input = aoc::reader(2015, 2, "input.txt");
+		b.iter(|| aoc1502::PartOne::solve(&input))
+	});
+}
+
+fn part_two_benchmark(c: &mut Criterion) {
+	c.bench_function("2015 day 2 part two", |b| {
+		let input = aoc::reader(2015, 2, "input.txt");
+		b.iter(|| aoc1502::PartTwo::solve(&input))
+	});
+}
+
+criterion_group!(benches, part_one_benchmark, part_two_benchmark);
+criterion_main!(benches);
