71b60c91689382bd9f34cf3ee3ff8d36f0453d50
Győri Sándor <alexaegis@gmail.com>
2019-06-09 16:12:34 +0200

    enabled strictFunctionTypes

--- src/2018/day06/typescript/tests/part_two.spec.ts
+++ src/2018/day06/typescript/tests/part_two.spec.ts
@@ -6,11 +6,11 @@ import { year, day, results, Args } from '..';
 describe(`${year} - Day ${day} - Part Two`, () => {
 	it(`should resolve to ${results.two.input} when using the input`, async () => {
 		const { input, args } = await reader<Args>(year, day)();
-		expect(await runner(input, args || { limit: 2 })).to.equal(results.two.input);
+		expect(await runner(input, args)).to.equal(results.two.input);
 	});
 
 	it(`should resolve to ${results.two.example} when using the example`, async () => {
 		const { input, args } = await reader<Args>(year, day, 'example.txt')();
-		expect(await runner(input, args || { limit: 2 })).to.equal(results.two.example);
+		expect(await runner(input, args)).to.equal(results.two.example);
 	});
 });
