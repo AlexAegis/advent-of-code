44f6d287cb66f531ab4107d9f82d81ebed2c02c3
Győri Sándor <alexaegis@gmail.com>
2019-06-09 20:44:11 +0200

    added 2015 day2 typescript

---
+++ src/2015/day02/typescript/tests/part_two.spec.ts
@@ -0,0 +1,18 @@
+import { read } from '@root';
+import { expect } from 'chai';
+import { day, results, year } from '..';
+import { runner } from '../part_two';
+
+describe(`${year} - Day ${day} - Part Two`, () => {
+	it(`should resolve to ${results.two.input} when using the input`, async () => {
+		expect(await runner((await read(year, day)()).input)).to.equal(results.two.input);
+	});
+
+	it('should be that that the first example resolves to 34', async () => {
+		expect(await runner('2x3x4')).to.equal(34);
+	});
+
+	it('should be that that the second example resolves to 14', async () => {
+		expect(await runner('1x1x10')).to.equal(14);
+	});
+});
