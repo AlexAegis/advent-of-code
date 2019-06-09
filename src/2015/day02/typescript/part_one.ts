02e9b562f85c466b897c8256e2fa0c32249fa50e
Győri Sándor <alexaegis@gmail.com>
2019-06-09 20:44:11 +0200

    added 2015 day2 typescript

---
+++ src/2015/day02/typescript/part_one.ts
@@ -0,0 +1,13 @@
+import { bench, min, read, split, sum } from '@root';
+import { day, year } from '.';
+
+export const runner = async (input: string) =>
+	split(input)
+		.map(line => line.split('x').map(c => Number(c)))
+		.map(([l, w, h]) => [l * w, w * h, h * l])
+		.map(sides => sides.reduce(min) + sides.reduce(sum) * 2)
+		.reduce(sum, 0);
+
+if (require.main === module) {
+	(async () => console.log(`Result: ${await bench(read(year, day), runner)}`))(); // 3803038 ~2ms
+}
