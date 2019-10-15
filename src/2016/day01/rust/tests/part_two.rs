031cd9c364ffdc5141e3a470f832694bec25ce5e
Győri Sándor <alexaegis@gmail.com>
2019-10-15 21:58:03 +0200

    coord tyoe

---
+++ src/2016/day01/rust/tests/part_two.rs
@@ -0,0 +1,14 @@
+extern crate aoc1601;
+
+use aoc::Solvable;
+
+#[test]
+fn input() {
+	let input = aoc::reader(2016, 1, "input.txt");
+	assert_eq!(aoc1601::PartTwo::solve(&input), 1795);
+}
+
+#[test]
+fn example_1() {
+	assert_eq!(aoc1601::PartTwo::solve(&")".to_string()), 1);
+}
