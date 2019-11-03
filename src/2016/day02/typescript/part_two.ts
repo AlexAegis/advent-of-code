68fe0bba51044a40e1bd2238329375dabd23555c
AlexAegis <alexaegis@gmail.com>
2019-11-03 18:23:11 +0100

    scaffolder launch option

---
+++ src/2016/day02/typescript/part_two.ts
@@ -0,0 +1,23 @@
+import { bench, read } from '@root';
+import { Coord } from '@root/meta/typescript/model/coord.class';
+import { Direction } from '@root/meta/typescript/model/direction.class';
+import { day, year } from '.';
+
+export const runner = (input: string) => {
+	const acc = { position: Coord.ORIGO, direction: Direction.NORTH, history: new Set<string>() };
+	loop: for (const next of input.split(', ')) {
+		if (next[0] === 'R') acc.direction = acc.direction.right();
+		if (next[0] === 'L') acc.direction = acc.direction.left();
+		for (let i = 0; i < Number(next.substring(1)); i++) {
+			acc.position.add(acc.direction);
+			const coordString = acc.position.toString();
+			if (acc.history.has(coordString)) break loop;
+			acc.history.add(coordString);
+		}
+	}
+	return acc.position.manhattan(Coord.ORIGO);
+};
+
+if (require.main === module) {
+	(async () => console.log(`Result: ${await bench(read(year, day), runner)}`))(); // 159 ~0.48ms
+}
---
+++ src/2016/day02/typescript/tests/part_two.spec.ts
@@ -0,0 +1,14 @@
+import { read } from '@root';
+import { expect } from 'chai';
+import { day, results, year } from '..';
+import { runner } from '../part_two';
+
+describe(`${year} - Day ${day} - Part Two`, () => {
+	it(`should resolve to ${results.two.input} when using the input`, async () => {
+		expect(await runner((await read(year, day)()).input)).to.equal(results.two.input);
+	});
+
+	it('should be that that the example resolves to 4', async () => {
+		expect(await runner('R8, R4, R4, R8')).to.equal(4);
+	});
+});
