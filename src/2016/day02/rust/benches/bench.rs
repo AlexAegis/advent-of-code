74c8ff443e165b82d63088e26d320d233e2da404
AlexAegis <alexaegis@gmail.com>
2019-11-03 18:23:11 +0100

    scaffolder launch option

---
+++ src/2016/day02/rust/benches/bench.rs
@@ -0,0 +1,25 @@
+#[macro_use]
+extern crate criterion;
+
+extern crate aoc;
+extern crate aoc1601;
+
+use aoc::Solvable;
+use criterion::Criterion;
+
+fn part_one_benchmark(c: &mut Criterion) {
+	c.bench_function("2016 day 1 part one", |b| {
+		let input = aoc::reader(2016, 1, "input.txt");
+		b.iter(|| aoc1601::PartOne::solve(&input))
+	});
+}
+
+fn part_two_benchmark(c: &mut Criterion) {
+	c.bench_function("2016 day 1 part two", |b| {
+		let input = aoc::reader(2016, 1, "input.txt");
+		b.iter(|| aoc1601::PartTwo::solve(&input))
+	});
+}
+
+criterion_group!(benches, part_one_benchmark, part_two_benchmark);
+criterion_main!(benches);
---
+++ src/2016/day02/rust/src/main.rs
@@ -0,0 +1,15 @@
+extern crate aoc;
+
+use aoc::Solvable;
+use aoc1601::{PartOne, PartTwo};
+
+pub fn main() {
+	let input = aoc::reader(2016, 1, "input.txt");
+	let result_part_one = PartOne::solve(&input); // 74, ~8ms
+	let result_part_two = PartTwo::solve(&input); // 1795, ~2ms
+
+	println!(
+		"Results for 2016 Day 1:\n\tPart One: {:?}\n\tPart Two: {:?}",
+		result_part_one, result_part_two
+	);
+}
