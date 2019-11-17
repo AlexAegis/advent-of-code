ce0d8d21f15a0c8ff76d440194b627e67714f3c7
AlexAegis <alexaegis@gmail.com>
2019-11-17 17:35:55 +0100

    fetched fiules done

--- scaffold/src/parser/mod.rs
+++ scaffold/src/parser/mod.rs
@@ -58,51 +58,67 @@ pub fn transform(html: &str, year: i16, day: i8) -> String {
 		current_task: 0,
 		year,
 		day,
+		p_count: 0,
 	};
 
-	main.children().map(|d| day.process(&d)).collect()
+	main.children().map(|d| day.process(&d, 0)).collect()
 }
 
 pub struct Day {
 	current_task: i8,
-	year: i32,
-	day: i16,
+	year: i16,
+	day: i8,
+	p_count: i8,
 }
 
 pub trait Processable {
-	fn process(&mut self, nr: &NodeRef<'_, scraper::node::Node>) -> String;
+	fn process(&mut self, nr: &NodeRef<'_, scraper::node::Node>, level: i32) -> String;
 }
 
 impl Processable for Day {
-	fn process(&mut self, nr: &NodeRef<'_, scraper::node::Node>) -> String {
+	fn process(&mut self, nr: &NodeRef<'_, scraper::node::Node>, level: i32) -> String {
 		let n: &Node = nr.value();
 		if let Some(t) = n.as_text() {
 			return t.chars().collect();
 		}
 		if let Some(t) = n.as_element() {
-			let wrapper = Wrapper::new(t.name());
-			let mut s = wrapper.prefix.to_owned();
-			for a in nr.children() {
-				s += &self.process(&a);
+			if t.name() == "p" && level == 0 {
+				self.p_count += 1;
 			}
-			s += wrapper.suffix;
-			if t.name() == "h2" {
-				self.current_task += 1;
-				s = s.replace("--- ", "");
-				s = s.replace(" ---", "");
-				s += "(https://adventofcode.com/";
-				s += &self.year.to_string();
-				s += "/day/";
-				s += &self.day.to_string();
-				s += ")\n\n";
-				if self.current_task == 1 {
-					s += "## [Part One](./part_one.ts)\n\n";
-				} else if self.current_task == 2 {
-					s += "## [Part Two](./part_two.ts)\n\n";
-				}
-			}
+			if level != 0 || t.name() != "p" || self.p_count <= 2 {
+				let wrapper = Wrapper::new(t.name());
 
-			return s;
+				let mut s = "".to_owned();
+
+				if self.current_task < 1 || t.name() != "h2" {
+					s += wrapper.prefix;
+					for a in nr.children() {
+						s += &self.process(&a, level + 1);
+					}
+					s += wrapper.suffix;
+				}
+
+				if t.name() == "h2" {
+					if self.current_task < 1 || t.name() != "h2" {
+						s = s.replace("--- ", "");
+						s = s.replace(" ---", "");
+						s += "(https://adventofcode.com/";
+						s += &self.year.to_string();
+						s += "/day/";
+						s += &self.day.to_string();
+						s += ")\n\n";
+					} else {
+						s += "\n";
+					}
+					self.current_task += 1;
+					if self.current_task == 1 {
+						s += "## [Part One](./part_one.ts)\n\n";
+					} else if self.current_task == 2 {
+						s += "## [Part Two](./part_two.ts)\n\n";
+					}
+				}
+				return s;
+			}
 		}
 
 		"".to_string()
