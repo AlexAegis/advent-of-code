c7f416adefabd872756f3414114e26919cdd1cf4
AlexAegis <alexaegis@gmail.com>
2019-11-18 20:26:13 +0100

    2015 day 5 part 2

--- src/2015/day05/typescript/tests/part_one.spec.ts
+++ src/2015/day05/typescript/tests/part_one.spec.ts
@@ -1,30 +1,30 @@
 import { read } from '@root';
 import { expect } from 'chai';
-import { day, inputs, results, year } from '..';
+import { day, results, year } from '..';
 import { runner } from '../part_one';
 
 describe(`${year} - Day ${day} - Part One`, () => {
 	it(`should resolve to ${results.one.input} when using the input`, async () => {
-		expect(await runner((await read(year, day)()).input)).to.equal(results.one.input);
+		expect(runner((await read(year, day)()).input)).to.equal(results.one.input);
 	});
 
 	it(`should true that example 1 is nice`, async () => {
-		expect(await runner(inputs.one.example.input)).to.equal(results.one.example);
+		expect(runner('ugknbfddgicrmopn')).to.equal(results.one.example);
 	});
 
 	it(`should true that example 2 is nice`, async () => {
-		expect(await runner((inputs.one.example2 && inputs.one.example2.input) || '')).to.equal(results.one.example2);
+		expect(runner('aaa')).to.equal(results.one.example2);
 	});
 
 	it(`should true that example 3 is naughty`, async () => {
-		expect(await runner((inputs.one.example3 && inputs.one.example3.input) || '')).to.equal(results.one.example3);
+		expect(runner('jchzalrnumimnmhp')).to.equal(results.one.example3);
 	});
 
 	it(`should true that example 4 is naughty`, async () => {
-		expect(await runner((inputs.one.example4 && inputs.one.example4.input) || '')).to.equal(results.one.example4);
+		expect(runner('haegwjzuvuyypxyu')).to.equal(results.one.example4);
 	});
 
 	it(`should true that example 5 is naughty`, async () => {
-		expect(await runner((inputs.one.example5 && inputs.one.example5.input) || '')).to.equal(results.one.example5);
+		expect(runner('dvszwmarrgswjxmb')).to.equal(results.one.example5);
 	});
 });
--- src/2015/day05/typescript/tests/part_two.spec.ts
+++ src/2015/day05/typescript/tests/part_two.spec.ts
@@ -5,6 +5,22 @@ import { runner } from '../part_two';
 
 describe(`${year} - Day ${day} - Part Two`, () => {
 	it(`should resolve to ${results.two.input} when using the input`, async () => {
-		expect(await runner((await read(year, day)()).input)).to.equal(results.two.input);
+		expect(runner((await read(year, day)()).input)).to.equal(results.two.input);
+	});
+
+	it(`should true that example 1 is nice`, async () => {
+		expect(runner('qjhvhtzxzqqjkmpb')).to.equal(true);
+	});
+
+	it(`should true that example 2 is nice`, async () => {
+		expect(runner('xxyxx')).to.equal(true);
+	});
+
+	it(`should true that example 3 is naughty`, async () => {
+		expect(runner('uurcxstgmygtbstg')).to.equal(false);
+	});
+
+	it(`should true that example 4 is naughty`, async () => {
+		expect(runner('ieodomkazucvgmuy')).to.equal(false);
 	});
 });
