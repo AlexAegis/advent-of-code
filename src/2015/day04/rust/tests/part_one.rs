b2f78c3930d5d008731fb5eba94fcf39838fd6c1
AlexAegis <alexaegis@gmail.com>
2019-11-18 20:57:39 +0100

    fixing tests

--- src/2015/day04/rust/tests/part_one.rs
+++ src/2015/day04/rust/tests/part_one.rs
@@ -3,11 +3,11 @@ use aoc::Solvable;
 #[test]
 fn input() {
 	let input = aoc::reader(2015, 4, "input.txt");
-	assert_eq!(aoc1504::PartOne::solve(&input), 0);
+	assert_eq!(aoc1504::PartOne::solve(&input), 346386);
 }
 
 #[test]
 fn example() {
 	let input = aoc::reader(2015, 4, "example.txt");
-	assert_eq!(aoc1504::PartOne::solve(&input), 0);
+	assert_eq!(aoc1504::PartOne::solve(&input), 609043);
 }
--- src/2015/day04/typescript/tests/part_two.spec.ts
+++ src/2015/day04/typescript/tests/part_two.spec.ts
@@ -1,18 +1,25 @@
 // import { read } from '@root';
 // import { expect } from 'chai';
+import * as isCI from 'is-ci';
 import { day, results, year } from '..';
 // import { runner } from '../part_two';
 
 describe(`${year} - Day ${day} - Part Two`, () => {
 	it(`should resolve to ${results.two.input} when using the input`, async () => {
-		// expect(await runner((await read(year, day)()).input)).to.equal(results.two.input);
+		if (!isCI) {
+			// expect(await runner((await read(year, day)()).input)).to.equal(results.two.input);
+		}
 	}).timeout(20000);
 
 	it(`should resolve to ${results.two.example} when using the example`, async () => {
-		// expect(await runner((await read(year, day, 'example.txt')()).input)).to.equal(results.two.example);
+		if (!isCI) {
+			// expect(await runner((await read(year, day, 'example.txt')()).input)).to.equal(results.two.example);
+		}
 	}).timeout(20000);
 
-	it(`should resolve to ${results.two.example} when using the example`, async () => {
-		// expect(await runner((await read(year, day, 'example.2.txt')()).input)).to.equal(results.two.example2);
+	it(`should resolve to ${results.two.example2} when using the example`, async () => {
+		if (!isCI) {
+			// expect(await runner((await read(year, day, 'example.2.txt')()).input)).to.equal(results.two.example2);
+		}
 	}).timeout(20000);
 });
