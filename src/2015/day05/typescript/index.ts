e214c861df61d7b66816f274a85624a9a04300be
AlexAegis <alexaegis@gmail.com>
2019-11-18 18:20:36 +0100

    2015 day5 scaffolded

---
+++ src/2015/day05/typescript/index.ts
@@ -0,0 +1,15 @@
+import { DayResults } from '@root';
+
+export const year = 2015;
+export const day = 5;
+
+export const results: DayResults = {
+	one: {
+		input: 0,
+		example: 0
+	},
+	two: {
+		input: 0,
+		example: 0
+	}
+};
---
+++ src/2015/day05/typescript/tests/part_one.spec.ts
@@ -0,0 +1,14 @@
+import { read } from '@root';
+import { expect } from 'chai';
+import { day, results, year } from '..';
+import { runner } from '../part_one';
+
+describe(`${year} - Day ${day} - Part One`, () => {
+	it(`should resolve to ${results.one.input} when using the input`, async () => {
+		expect(await runner((await read(year, day)()).input)).to.equal(results.one.input);
+	});
+
+	it(`should resolve to ${results.one.example} when using the example`, async () => {
+		expect(await runner((await read(year, day, 'example.txt')()).input)).to.equal(results.one.example);
+	});
+});
