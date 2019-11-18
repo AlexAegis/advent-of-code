40dd020d6f5c7885cf7beed599019ec722260aba
AlexAegis <alexaegis@gmail.com>
2019-11-18 18:53:39 +0100

    exots

--- src/2015/day05/typescript/index.ts
+++ src/2015/day05/typescript/index.ts
@@ -1,15 +1,62 @@
-import { DayResults } from '@root';
+import { DayInputs, DayResults } from '@root';
 
 export const year = 2015;
 export const day = 5;
 
-export const results: DayResults = {
+export const inputs: DayInputs<string> = {
 	one: {
-		input: 0,
-		example: 0
+		input: {
+			input: ''
+		},
+		example: {
+			input: 'ugknbfddgicrmopn'
+		},
+		example2: {
+			input: 'aaa'
+		},
+		example3: {
+			input: 'jchzalrnumimnmhp'
+		},
+		example4: {
+			input: 'haegwjzuvuyypxyu'
+		},
+		example5: {
+			input: 'dvszwmarrgswjxmb'
+		}
+	},
+	two: {
+		input: {
+			input: ''
+		},
+		example: {
+			input: 'qjhvhtzxzqqjkmpb'
+		},
+		example2: {
+			input: 'xxyxx'
+		},
+		example3: {
+			input: 'uurcxstgmygtbstg'
+		},
+		example4: {
+			input: 'ieodomkazucvgmuy'
+		}
+	}
+};
+
+export const results: DayResults<number | boolean> = {
+	one: {
+		input: 236,
+		example1: true,
+		example2: true,
+		example3: false,
+		example4: false,
+		example5: false
 	},
 	two: {
 		input: 0,
-		example: 0
+		example1: true,
+		example2: true,
+		example3: false,
+		example4: false
 	}
 };
