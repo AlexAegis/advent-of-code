47b31c288c31f5a57d1950c622d4e0e47a99d9e7
AlexAegis <alexaegis@gmail.com>
2019-11-19 08:40:21 +0100

    increased timeout

--- src/2018/day05/typescript/tests/part_two.spec.ts
+++ src/2018/day05/typescript/tests/part_two.spec.ts
@@ -6,9 +6,9 @@ import { runner } from '../part_two';
 describe(`${year} - Day ${day} - Part Two`, () => {
 	it(`should resolve to ${results.two.input} when using the input`, async () => {
 		expect(await runner((await read(year, day)()).input)).to.equal(results.two.input);
-	});
+	}).timeout(10000);
 
 	it(`should resolve to ${results.two.example} when using the example`, async () => {
 		expect(await runner((await read(year, day, 'example.txt')()).input)).to.equal(results.two.example);
-	});
+	}).timeout(10000);
 });
