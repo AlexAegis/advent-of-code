c7f2eb3aecf48aa69de5ec5d94ce6188585f9d99
AlexAegis <alexaegis@gmail.com>
2019-11-20 18:59:34 +0100

    bench

--- src/2015/day02/rust/src/lib.rs
+++ src/2015/day02/rust/src/lib.rs
@@ -1,29 +1,29 @@
 pub struct PartOne;
 pub struct PartTwo;
 
-impl aoc::Solvable<&str, i32> for PartOne {
-	fn solve(input: &str) -> aoc::Solution<i32> {
+impl aoc::Solvable<&str, u32> for PartOne {
+	fn solve(input: &str) -> aoc::Solution<u32> {
 		Ok(input
 			.lines()
 			.map(|l| l.trim())
-			.map(|line| line.split('x').map(|c| c.parse::<i32>().unwrap()).collect())
-			.map(|arr: Vec<i32>| [arr[0] * arr[1], arr[1] * arr[2], arr[2] * arr[0]])
-			.map(|sides| sides.iter().min().unwrap() + sides.iter().sum::<i32>() * 2)
-			.sum::<i32>())
+			.map(|line| line.split('x').map(|c| c.parse().unwrap()).collect())
+			.map(|arr: Vec<u32>| [arr[0] * arr[1], arr[1] * arr[2], arr[2] * arr[0]])
+			.map(|sides| sides.iter().min().unwrap() + sides.iter().sum::<u32>() * 2)
+			.sum())
 	}
 }
 
-impl aoc::Solvable<&str, i32> for PartTwo {
-	fn solve(input: &str) -> aoc::Solution<i32> {
+impl aoc::Solvable<&str, u32> for PartTwo {
+	fn solve(input: &str) -> aoc::Solution<u32> {
 		Ok(input
 			.lines()
 			.map(|l| l.trim())
-			.map(|line| line.split('x').map(|c| c.parse::<i32>().unwrap()).collect())
-			.map(|mut s: Vec<i32>| {
+			.map(|line| line.split('x').map(|c| c.parse().unwrap()).collect())
+			.map(|mut s: Vec<u32>| {
 				s.sort_unstable();
 				s
 			})
-			.map(|s| s.iter().product::<i32>() + s.iter().take(2).map(|n| n * 2).sum::<i32>())
-			.sum::<i32>())
+			.map(|s| s.iter().product::<u32>() + s.iter().take(2).map(|n| n * 2).sum::<u32>())
+			.sum())
 	}
 }
