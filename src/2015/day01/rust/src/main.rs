d945b01a38e5cbda2fa248770bcd0b7ea15e9a98
AlexAegis <alexaegis@gmail.com>
2019-11-20 20:19:25 +0100

    new benches

--- src/2015/day01/rust/src/main.rs
+++ src/2015/day01/rust/src/main.rs
@@ -3,8 +3,8 @@ use aoc1501::{PartOne, PartTwo};
 
 pub fn main() -> aoc::Result<()> {
 	let input = aoc::reader(2015, 1, "input.txt")?;
-	let result_part_one = PartOne::solve(&input)?; // 74, ~8.3μs
-	let result_part_two = PartTwo::solve(&input)?; // 1795, ~2.5μs
+	let result_part_one = PartOne::solve(&input)?; // 74, ~6.2μs
+	let result_part_two = PartTwo::solve(&input)?; // 1795, ~1.9μs
 
 	println!(
 		"Results for 2015 Day 1:\n\tPart One: {:?}\n\tPart Two: {:?}",
