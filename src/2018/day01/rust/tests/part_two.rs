8ecad2c75f1e27c9fa6533577a968098ea894e45
AlexAegis <alexaegis@gmail.com>
2019-11-30 17:13:24 +0100

    18 -> 2018

--- src/2018/day01/rust/tests/part_two.rs
+++ src/2018/day01/rust/tests/part_two.rs
@@ -3,13 +3,13 @@ use aoclib::Solvable;
 #[test]
 fn input() -> aoclib::Result<()> {
 	let input = aoclib::reader(2018, 1, "input.txt")?;
-	assert_eq!(aoc1801::PartTwo::solve(&input)?, 55250);
+	assert_eq!(aoc201801::PartTwo::solve(&input)?, 55250);
 	Ok(())
 }
 
 #[test]
 fn example_1() -> aoclib::Result<()> {
 	let input = aoclib::reader(2018, 1, "example.txt")?;
-	assert_eq!(aoc1801::PartTwo::solve(&input)?, 10);
+	assert_eq!(aoc201801::PartTwo::solve(&input)?, 10);
 	Ok(())
 }
