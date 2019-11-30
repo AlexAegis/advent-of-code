4361f6200f3af1d5823e393d7f7b62bfd36c8358
AlexAegis <alexaegis@gmail.com>
2019-11-30 17:13:24 +0100

    18 -> 2018

--- src/2018/day01/rust/benches/bench.rs
+++ src/2018/day01/rust/benches/bench.rs
@@ -7,14 +7,14 @@ use criterion::Criterion;
 fn part_one_benchmark(c: &mut Criterion) {
 	c.bench_function("2018 day 1 part one", |b| {
 		let input = aoclib::reader(2018, 1, "input.txt").unwrap();
-		b.iter(|| aoc1801::PartOne::solve(&input).unwrap())
+		b.iter(|| aoc201801::PartOne::solve(&input).unwrap())
 	});
 }
 
 fn part_two_benchmark(c: &mut Criterion) {
 	c.bench_function("2018 day 1 part two", |b| {
 		let input = aoclib::reader(2018, 1, "input.txt").unwrap();
-		b.iter(|| aoc1801::PartTwo::solve(&input).unwrap())
+		b.iter(|| aoc201801::PartTwo::solve(&input).unwrap())
 	});
 }
 
--- src/2018/day01/rust/tests/part_one.rs
+++ src/2018/day01/rust/tests/part_one.rs
@@ -3,13 +3,13 @@ use aoclib::Solvable;
 #[test]
 fn input() -> aoclib::Result<()> {
 	let input = aoclib::reader(2018, 1, "input.txt")?;
-	assert_eq!(aoc1801::PartOne::solve(&input)?, 408);
+	assert_eq!(aoc201801::PartOne::solve(&input)?, 408);
 	Ok(())
 }
 
 #[test]
 fn example() -> aoclib::Result<()> {
 	let input = aoclib::reader(2018, 1, "example.txt")?;
-	assert_eq!(aoc1801::PartOne::solve(&input)?, 4);
+	assert_eq!(aoc201801::PartOne::solve(&input)?, 4);
 	Ok(())
 }
