7b000434d76604dd77e8bd35418275581e1fcfed
AlexAegis <alexaegis@gmail.com>
2019-12-01 06:36:19 +0100

    added 2019 day 1 rust solution

--- src/2019/day01/rust/src/main.rs
+++ src/2019/day01/rust/src/main.rs
@@ -1,10 +1,10 @@
-use aoclib::Solvable;
 use aoc201901::{PartOne, PartTwo};
+use aoclib::Solvable;
 
 pub fn main() -> aoclib::Result<()> {
 	let input = aoclib::reader(2019, 1, "input.txt")?;
-	let result_part_one = PartOne::solve(&input)?; // 0, ~0μs
-	let result_part_two = PartTwo::solve(&input)?; // 0, ~0μs
+	let result_part_one = PartOne::solve(&input)?; // 3399947, ~1.6μs
+	let result_part_two = PartTwo::solve(&input)?; // 5097039, ~3.6μs
 
 	println!(
 		"Results for 2019 Day 1:\n\tPart One: {:?}\n\tPart Two: {:?}",
