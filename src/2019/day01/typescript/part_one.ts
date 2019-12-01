24a8d8071c46148d5185219dc512a0168f01aa6c
AlexAegis <alexaegis@gmail.com>
2019-12-01 05:55:53 +0100

    regexfix

--- src/2019/day01/typescript/part_one.ts
+++ src/2019/day01/typescript/part_one.ts
@@ -2,7 +2,16 @@ import { bench, read } from '@lib';
 import { day, year } from '.';
 
 export const runner = async (input: string) => {
-	return 0;
+	// const nums = [...input].filter(c => /^(\+|-)?[0-9]+/.test(c)).map(c => Number(c)); // one line
+	const nums = input.split(/\r?\n/).map(s => Number(s)); // multiline
+
+	let sum = 0;
+	for (let i = 0; i < nums.length; i++) {
+		if (nums[i] === nums[(i + 1) % nums.length]) {
+			sum += nums[i];
+		}
+	}
+	return sum;
 };
 
 if (require.main === module) {
