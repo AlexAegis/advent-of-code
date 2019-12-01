ac48d21c23bed88894f82ed3cf37b56491d4f7d7
AlexAegis <alexaegis@gmail.com>
2019-12-01 05:40:22 +0100

    2019 day1 prepare

---
+++ src/2019/day01/rust/src/lib.rs
@@ -0,0 +1,14 @@
+pub struct PartOne;
+pub struct PartTwo;
+
+impl aoclib::Solvable<&str, i32> for PartOne {
+	fn solve(input: &str) -> aoclib::Solution<i16> {
+		Ok(0)
+	}
+}
+
+impl aoclib::Solvable<&str, i32> for PartTwo {
+	fn solve(input: &str) -> aoclib::Solution<i16> {
+		Ok(0)
+	}
+}
