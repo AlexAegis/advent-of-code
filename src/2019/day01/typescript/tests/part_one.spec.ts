ff489c85cf6138e858eb66a0c5b0c6e2cf4e5453
AlexAegis <alexaegis@gmail.com>
2019-12-01 06:25:08 +0100

    added tests

--- src/2019/day01/typescript/tests/part_one.spec.ts
+++ src/2019/day01/typescript/tests/part_one.spec.ts
@@ -8,7 +8,19 @@ describe(`${year} - Day ${day} - Part One`, () => {
 		expect(await runner((await read(year, day)()).input)).to.equal(results.one.input);
 	});
 
-	it('should be that that both the first examples resolves to 0', async () => {
-		expect(await runner('')).to.equal(0);
+	it('should be that that the first example resolves to 2', async () => {
+		expect(await runner('12')).to.equal(2);
+	});
+
+	it('should be that that the second example resolves to 2', async () => {
+		expect(await runner('14')).to.equal(2);
+	});
+
+	it('should be that that the third example resolves to 654', async () => {
+		expect(await runner('1969')).to.equal(654);
+	});
+
+	it('should be that that the fourth example resolves to 33583', async () => {
+		expect(await runner('100756')).to.equal(33583);
 	});
 });
