27e9eb28a3b5b6375eecefe281cc608e7183f77a
AlexAegis <alexaegis@gmail.com>
2019-12-01 06:18:11 +0100

    2019 day 1 ts

--- src/2019/day01/typescript/part_two.ts
+++ src/2019/day01/typescript/part_two.ts
@@ -1,11 +1,21 @@
-// import { bench, read } from '@lib';
-// import { day, year } from '.';
+import { bench, read } from '@lib';
+import { day, year } from '.';
 
-export const runner = async (_input: string) => {
-	return 0;
+const fuel = (i: number): number => {
+	const f = Math.max(Math.floor(i / 3) - 2, 0);
+	let m = 0;
+	if (f > 0) {
+		m = fuel(f);
+	}
+	return f + m;
 };
 
+export const runner = async (input: string) =>
+	input
+		.split(/\r?\n/)
+		.map(n => fuel(Number(n)))
+		.reduce((s, n) => s + n, 0);
+
 if (require.main === module) {
-	(async () => console.log(`Result: ${await runner('')}`))();
-	// (async () => console.log(`Result: ${await bench(read(year, day), runner)}`))(); // 0 ~0ms
+	(async () => console.log(`Result: ${await bench(read(year, day), runner)}`))(); // 5097039 ~0.38ms
 }
