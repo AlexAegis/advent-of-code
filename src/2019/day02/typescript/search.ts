c8db8ab010306c876cec3a0e8cea9936cb2aaf9f
AlexAegis <alexaegis@gmail.com>
2019-12-02 06:57:12 +0100

    2019 02 typescript

---
+++ src/2019/day02/typescript/search.ts
@@ -0,0 +1,29 @@
+export const search = (a: number[], noun?: number, verb?: number, immutable = true): number => {
+	const arr = immutable ? [...a] : a;
+	if (noun !== undefined) {
+		arr[1] = noun;
+	}
+	if (verb !== undefined) {
+		arr[2] = verb;
+	}
+	let i = 0;
+	while (true) {
+		const curr = arr[i % arr.length];
+
+		if (curr === 99) {
+			break;
+		}
+
+		if (curr === 1) {
+			arr[arr[(i + 3) % arr.length]] = arr[arr[(i + 1) % arr.length]] + arr[arr[(i + 2) % arr.length]];
+		}
+
+		if (curr === 2) {
+			arr[arr[(i + 3) % arr.length]] = arr[arr[(i + 1) % arr.length]] * arr[arr[(i + 2) % arr.length]];
+		}
+
+		i = (i + 4) % arr.length;
+	}
+
+	return arr[0];
+};
