e63d89cdf621d1ac7702f27dbba6176c6f22af19
AlexAegis <alexaegis@gmail.com>
2019-12-02 06:34:00 +0100

    initial 2019 01 typescript solution

---
+++ src/2019/day02/typescript/index.ts
@@ -0,0 +1,13 @@
+import { DayResults } from '@lib';
+
+export const year = 2019;
+export const day = 2;
+
+export const results: DayResults = {
+	one: {
+		input: 3101844
+	},
+	two: {
+		input: 8478
+	}
+};
