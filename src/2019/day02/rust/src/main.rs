bf1e98d2235ffd9e3cd310c5ecc391c07cbe529a
AlexAegis <alexaegis@gmail.com>
2019-12-02 23:13:52 +0100

    2019 02 Rust

--- src/2019/day02/rust/src/main.rs
+++ src/2019/day02/rust/src/main.rs
@@ -1,10 +1,10 @@
-use aoclib::Solvable;
 use aoc201902::{PartOne, PartTwo};
+use aoclib::Solvable;
 
 pub fn main() -> aoclib::Result<()> {
 	let input = aoclib::reader(2019, 2, "input.txt")?;
-	let result_part_one = PartOne::solve(&input)?; // 0, ~0μs
-	let result_part_two = PartTwo::solve(&input)?; // 0, ~0μs
+	let result_part_one = PartOne::solve(&input)?; // 3101844, ~2.2μs
+	let result_part_two = PartTwo::solve(&input)?; // 8478, ~1ms
 
 	println!(
 		"Results for 2019 Day 2:\n\tPart One: {:?}\n\tPart Two: {:?}",
--- src/2019/day02/rust/tests/part_two.rs
+++ src/2019/day02/rust/tests/part_two.rs
@@ -3,12 +3,6 @@ use aoclib::Solvable;
 #[test]
 fn input() -> aoclib::Result<()> {
 	let input = aoclib::reader(2019, 2, "input.txt")?;
-	assert_eq!(aoc201902::PartTwo::solve(&input)?, 0);
-	Ok(())
-}
-
-#[test]
-fn example_1() -> aoclib::Result<()> {
-	assert_eq!(aoc201902::PartTwo::solve("")?, 0);
+	assert_eq!(aoc201902::PartTwo::solve(&input)?, 8478);
 	Ok(())
 }
