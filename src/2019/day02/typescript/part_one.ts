0c2bbaf616e025548e3f16954128832c536228ef
AlexAegis <alexaegis@gmail.com>
2019-12-02 07:01:41 +0100

    renamed to compute

--- src/2019/day02/typescript/part_one.ts
+++ src/2019/day02/typescript/part_one.ts
@@ -1,12 +1,9 @@
 import { bench, read } from '@lib';
 import { day, year } from '.';
+import { compute } from './compute';
 import { parse } from './parse';
-import { search } from './search';
 
-export const runner = (input: string) => {
-	const arr = parse(input);
-	return search(arr, 12, 2, false);
-};
+export const runner = (input: string) => compute(parse(input), 12, 2, false);
 
 if (require.main === module) {
 	(async () => console.log(`Result: ${await bench(read(year, day), runner)}`))(); // 3101844 ~0.25ms
