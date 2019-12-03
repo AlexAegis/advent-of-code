cc7f8046333c25db0909bc45a97172f4e04ef6c8
AlexAegis <alexaegis@gmail.com>
2019-12-03 05:46:10 +0100

    scaffold 2019 3

---
+++ src/2019/day03/typescript/tests/part_one.spec.ts
@@ -0,0 +1,14 @@
+import { read } from '@lib';
+import { expect } from 'chai';
+import { day, results, year } from '..';
+import { runner } from '../part_one';
+
+describe(`${year} - Day ${day} - Part One`, () => {
+	it(`should resolve to ${results.one.input} when using the input`, async () => {
+		expect(await runner((await read(year, day)()).input)).to.equal(results.one.input);
+	});
+
+	it('should be that that the first example resolves to 0', async () => {
+		expect(await runner('')).to.equal(0);
+	});
+});
