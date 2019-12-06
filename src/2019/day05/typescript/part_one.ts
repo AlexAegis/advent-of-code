918069bd5f2a29e697fa603264103e4ef01a0c46
AlexAegis <alexaegis@gmail.com>
2019-12-06 05:55:46 +0100

    intcode

--- src/2019/day05/typescript/part_one.ts
+++ src/2019/day05/typescript/part_one.ts
@@ -2,14 +2,9 @@ import { bench, read } from '@lib';
 import { day, year } from '.';
 import { parseCommaSeparatedNumbers } from './parse';
 
-export const compute = (a: number[], noun?: number, verb?: number, immutable = true): number => {
+export const compute = (a: number[], input?: number, immutable = true): number => {
 	const arr = immutable ? [...a] : a;
-	if (noun !== undefined) {
-		arr[1] = noun;
-	}
-	if (verb !== undefined) {
-		arr[2] = verb;
-	}
+
 	let i = 0;
 	let result = 0;
 	while (true) {
@@ -38,38 +33,23 @@ export const compute = (a: number[], noun?: number, verb?: number, immutable = t
 			paramMode.length <= 1 || paramMode[1] === 0 ? arr[arr[(i + 2) % arr.length]] : arr[(i + 2) % arr.length];
 
 		if (instruction === 1) {
-			if (paramMode.length <= 2 || paramMode[2] === 0) {
-				arr[arr[(i + 3) % arr.length]] = paramA + paramB;
-			} else {
-				arr[(i + 3) % arr.length] = paramA + paramB;
-			}
+			arr[arr[(i + 3) % arr.length]] = paramA + paramB;
 		}
 
 		if (instruction === 2) {
-			if (paramMode.length <= 2 || paramMode[2] === 0) {
-				// Parameters that an instruction writes to will never be in immediate mode.
-				arr[arr[(i + 3) % arr.length]] = paramA * paramB;
-			} else {
-				arr[(i + 3) % arr.length] = paramA * paramB;
-			}
+			arr[arr[(i + 3) % arr.length]] = paramA * paramB;
 		}
 
 		if (instruction === 3) {
-			if (paramMode.length <= 1 || paramMode[1] === 0) {
-				arr[arr[(i + 2) % arr.length]] = paramA;
-			} else {
-				arr[(i + 2) % arr.length] = paramA;
+			if (input) {
+				arr[arr[(i + 2) % arr.length]] = input;
 			}
+
 			step = 2;
 		}
 
 		if (instruction === 4) {
 			result = paramA;
-			if (paramMode.length <= 1 || paramMode[0] === 0) {
-				arr[arr[(i + 1) % arr.length]] = paramA;
-			} else {
-				arr[(i + 1) % arr.length] = paramA; // arr[paramA] ?
-			}
 			step = 2;
 		}
 
@@ -81,12 +61,12 @@ export const compute = (a: number[], noun?: number, verb?: number, immutable = t
 
 export const runner = async (input: string) => {
 	const a = parseCommaSeparatedNumbers(input);
-	const s = compute(a, 1, undefined, false);
+	const s = compute(a, 1, false);
 
 	return s;
 };
 
 if (require.main === module) {
 	// (async () => console.log(`Result: ${await runner('1002,4,3,4,33')}`))();
-	(async () => console.log(`Result: ${await bench(read(year, day), runner)}`))(); // 0 ~0ms
+	(async () => console.log(`Result: ${await bench(read(year, day), runner)}`))(); // 16348437 ~0ms
 }
