7b95d4c3338f53ca574e2033fa03a3fa32f195b1
AlexAegis <alexaegis@gmail.com>
2019-12-07 00:33:50 +0100

    restored day 6

---
+++ src/2019/day06/typescript/tests/part_two.spec.ts
@@ -0,0 +1,14 @@
+import { read } from '@lib';
+import { expect } from 'chai';
+import { day, results, year } from '..';
+import { runner } from '../part_two';
+
+describe(`${year} - Day ${day} - Part Two`, () => {
+	it(`should resolve to ${results.two.input} when using the input`, async () => {
+		expect(await runner((await read(year, day)()).input)).to.equal(results.two.input);
+	});
+
+	it('should be that that the first example resolves to 4', async () => {
+		expect(await runner('COM)B\nB)C\nC)D\nD)E\nE)F\nB)G\nG)H\nD)I\nE)J\nJ)K\nK)L\nK)YOU\nI)SAN')).to.equal(4);
+	});
+});
