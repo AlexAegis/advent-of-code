39c05a39ea16af41817d2e1303ce77e5a41a7ef6
AlexAegis <alexaegis@gmail.com>
2019-12-07 00:25:52 +0100

    intcode tasks refactored

--- src/2019/day02/typescript/tests/part_one.spec.ts
+++ src/2019/day02/typescript/tests/part_one.spec.ts
@@ -1,7 +1,7 @@
 import { read } from '@lib';
+import { IntCodeComputer } from '@lib/intcode';
 import { expect } from 'chai';
 import { day, results, year } from '..';
-import { compute } from '../compute';
 import { parse } from '../parse';
 import { runner } from '../part_one';
 
@@ -11,22 +11,32 @@ describe(`${year} - Day ${day} - Part One`, () => {
 	});
 
 	it('should be that that the first example resolves to 3500', async () => {
-		expect(await compute(parse('1,9,10,3,2,3,11,0,99,30,40,50'), undefined, undefined, false)).to.equal(3500);
+		const computer = new IntCodeComputer(parse('1,9,10,3,2,3,11,0,99,30,40,50'), true);
+		computer.execute();
+		expect(computer.peek(0)).to.equal(3500);
 	});
 
 	it('should be that that the second example resolves to 2', async () => {
-		expect(await compute(parse('1,0,0,0,99'), undefined, undefined, false)).to.equal(2);
+		const computer = new IntCodeComputer(parse('1,0,0,0,99'), true);
+		computer.execute();
+		expect(computer.peek(0)).to.equal(2);
 	});
 
 	it('should be that that the third example resolves to 2', async () => {
-		expect(await compute(parse('2,3,0,3,99'), undefined, undefined, false)).to.equal(2);
+		const computer = new IntCodeComputer(parse('2,3,0,3,99'), true);
+		computer.execute();
+		expect(computer.peek(0)).to.equal(2);
 	});
 
 	it('should be that that the fourth example resolves to 2', async () => {
-		expect(await compute(parse('2,4,4,5,99,0'), undefined, undefined, false)).to.equal(2);
+		const computer = new IntCodeComputer(parse('2,4,4,5,99,0'), true);
+		computer.execute();
+		expect(computer.peek(0)).to.equal(2);
 	});
 
 	it('should be that that the fifth example resolves to 30', async () => {
-		expect(await compute(parse('1,1,1,4,99,5,6,0,99'), undefined, undefined, false)).to.equal(30);
+		const computer = new IntCodeComputer(parse('1,1,1,4,99,5,6,0,99'), true);
+		computer.execute();
+		expect(computer.peek(0)).to.equal(30);
 	});
 });
---
+++ src/lib/typescript/intcode/instruction.enum.ts
@@ -0,0 +1,24 @@
+export enum Instruction {
+	ADD = 1,
+	MUL = 2,
+	IN = 3,
+	OUT = 4,
+	JIT = 5,
+	JIF = 6,
+	LT = 7,
+	EQ = 8,
+	HALT = 99
+}
+
+export const isInstruction = (n: number): boolean => {
+	return (n >= 1 && n <= 8) || n === 99;
+};
+
+export const toInstruction = (code: number): Instruction => {
+	const inst = code % 100;
+	if (isInstruction(inst)) {
+		return inst;
+	} else {
+		throw new Error(`Unsupported instruction ${inst}`);
+	}
+};
