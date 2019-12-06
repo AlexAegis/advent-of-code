634beebec35f6e2eb98171ad3bf58bf9177e0a76
AlexAegis <alexaegis@gmail.com>
2019-12-07 00:33:50 +0100

    restored day 6

---
+++ src/2019/day06/typescript/index.ts
@@ -0,0 +1,13 @@
+import { DayResults } from '@lib';
+
+export const year = 2019;
+export const day = 6;
+
+export const results: DayResults = {
+	one: {
+		input: 223251
+	},
+	two: {
+		input: 430
+	}
+};
