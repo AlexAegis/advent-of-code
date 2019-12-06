5442c728dfe56ad8f4f22b3c688dbd3d816c45ca
AlexAegis <alexaegis@gmail.com>
2019-12-07 00:33:50 +0100

    restored day 6

---
+++ src/2019/day06/typescript/model/planet.class.ts
@@ -0,0 +1,40 @@
+export enum Notable {
+	CENTER = 'COM',
+	YOU = 'YOU',
+	SAN = 'SAN'
+}
+
+export class Planet implements Iterable<Planet> {
+	public orbiters: Planet[] = [];
+	public constructor(public name: string) {}
+
+	public *[Symbol.iterator](): IterableIterator<Planet> {
+		for (const orbiter of this.orbiters) {
+			yield* orbiter;
+			yield orbiter;
+		}
+	}
+
+	public *find(planet: string): IterableIterator<Planet> {
+		if (this.name === planet) {
+			yield this;
+		}
+		for (const orbiter of this.orbiters) {
+			yield* orbiter.find(planet);
+		}
+	}
+
+	// TODO: Refactor this so it won't use find
+	public *reach(planet: string): IterableIterator<Planet> {
+		if (this.name === planet || !this.find(planet).next().done) {
+			yield this;
+		}
+		for (const orbiter of this.orbiters) {
+			yield* orbiter.reach(planet);
+		}
+	}
+
+	public toString(): string {
+		return `${this.name} o: ${this.orbiters.map(o => o.name)}`;
+	}
+}
