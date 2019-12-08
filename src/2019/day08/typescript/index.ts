57966cc21231f64aad949c0b214a3ce0f7880387
AlexAegis <alexaegis@gmail.com>
2019-12-08 06:38:50 +0100

    2019 8 typescript

--- src/2019/day08/typescript/index.ts
+++ src/2019/day08/typescript/index.ts
@@ -3,11 +3,11 @@ import { DayResults } from '@lib';
 export const year = 2019;
 export const day = 8;
 
-export const results: DayResults = {
+export const results: DayResults<number, string> = {
 	one: {
-		input: 0
+		input: 1088
 	},
 	two: {
-		input: 0
+		input: 'LGYHB'
 	}
 };
