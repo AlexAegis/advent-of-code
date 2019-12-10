867d074dedfd08c73d55cfee1a144dc2804904b2
AlexAegis <alexaegis@gmail.com>
2019-12-10 09:36:05 +0100

    first solv

--- src/2019/day10/typescript/part_one.ts
+++ src/2019/day10/typescript/part_one.ts
@@ -1,11 +1,30 @@
 import { bench, read } from '@lib';
+import { Coord } from '@lib/model';
 import { day, year } from '.';
+import { Field, FieldType, intoMap, parseLines } from './parse';
 
+export const mostLos = (flat: Field[]): Field => {
+	flat.forEach(f => {
+		f.lineOfSight = flat
+			.filter(fo => !fo.pos.equal(f.pos))
+			.map(o => [...f.pos.reach(o.pos)].filter(l => flat.find(fl => fl.pos.equal(l))).length)
+			.filter(n => n === 0).length;
+	});
+	return flat.reduce((a, n) => (n.lineOfSight > a.lineOfSight ? n : a), flat[0]);
+};
 export const runner = async (input: string) => {
-	return 0;
+	const p = parseLines(input);
+
+	const map = intoMap(p);
+	const flat = [...map.values()];
+
+	return mostLos(flat).lineOfSight;
 };
 
 if (require.main === module) {
-	(async () => console.log(`Result: ${await runner('')}`))();
-	// (async () => console.log(`Result: ${await bench(read(year, day), runner)}`))(); // 0 ~0ms
+	(async () => console.log(`Result: ${await bench(read(year, day), runner)}`))(); // 230 ~0ms
+	// (async () => console.log(`Result: ${await bench(read(year, day, 'example.1.txt'), runner)}`))(); // 8 ~0ms
+	// (async () => console.log(`Result: ${await bench(read(year, day, 'example.2.txt'), runner)}`))(); // 33 ~0ms
+	// (async () => console.log(`Result: ${await bench(read(year, day, 'example.3.txt'), runner)}`))(); // 35 ~0ms
+	// (async () => console.log(`Result: ${await bench(read(year, day, 'example.4.txt'), runner)}`))(); // 41 ~0ms// (async () => console.log(`Result: ${await bench(read(year, day, 'example.5.txt'), runner)}`))(); // 210 ~0ms
 }
