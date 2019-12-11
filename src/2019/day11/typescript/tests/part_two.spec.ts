02889b7892b23341c735a5316dce75823a42af82
AlexAegis <alexaegis@gmail.com>
2019-12-11 07:33:47 +0100

    2019 day 11 typescript

---
+++ src/2019/day11/typescript/tests/part_two.spec.ts
@@ -0,0 +1,11 @@
+// import { read } from '@lib';
+// import { expect } from 'chai';
+import { day, results, year } from '..';
+// import { runner } from '../part_two';
+
+describe(`${year} - Day ${day} - Part Two`, () => {
+	// TODO: ASCII ART PARSER
+	it(`should resolve to ${results.two.input} when using the input`, async () => {
+		// expect(await runner((await read(year, day)()).input)).to.equal(results.two.input);
+	});
+});
