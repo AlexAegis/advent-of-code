99619866b0dcc670e777e17111dbe80ef95fe116
AlexAegis <alexaegis@gmail.com>
2019-12-11 07:33:47 +0100

    2019 day 11 typescript

---
+++ src/2019/day11/rust/tests/part_one.rs
@@ -0,0 +1,14 @@
+use aoclib::Solvable;
+
+#[test]
+fn input() -> aoclib::Result<()> {
+	let input = aoclib::reader(2019, 11, "input.txt")?;
+	assert_eq!(aoc201911::PartOne::solve(&input)?, 0);
+	Ok(())
+}
+
+#[test]
+fn example_1() -> aoclib::Result<()> {
+	assert_eq!(aoc201911::PartOne::solve("")?, 0);
+	Ok(())
+}
