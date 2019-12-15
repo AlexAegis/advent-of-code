29076d56a95a997b11821349e33ebc8e6bad42d0
AlexAegis <alexaegis@gmail.com>
2019-12-15 07:57:14 +0100

    pathing proto

---
+++ src/2019/day15/typescript/meta.ts
@@ -0,0 +1,70 @@
+import { drawMapStatic, printMatrix } from '@lib/functions';
+import { Direction } from '@lib/model';
+
+export enum Move {
+	NORTH = 1,
+	SOUTH = 2,
+	WEST = 3,
+	EAST = 4
+}
+
+export const getDirection = (move: Move): Direction => {
+	switch (move) {
+		case Move.NORTH:
+			return Direction.NORTH;
+		case Move.SOUTH:
+			return Direction.SOUTH;
+		case Move.EAST:
+			return Direction.EAST;
+		case Move.WEST:
+			return Direction.WEST;
+		default:
+			return Direction.NONE;
+	}
+};
+
+export const getMove = (direction: Direction): Move => {
+	if (direction.equals(Direction.NORTH)) {
+		return Move.NORTH;
+	} else if (direction.equals(Direction.EAST)) {
+		return Move.EAST;
+	} else if (direction.equals(Direction.SOUTH)) {
+		return Move.SOUTH;
+	} else if (direction.equals(Direction.WEST)) {
+		return Move.WEST;
+	} else {
+		throw new Error('Not a direction: ' + direction.toString());
+	}
+};
+
+export enum Status {
+	WALL = 0,
+	MOVED = 1,
+	FINISHED = 2
+}
+
+export enum Tile {
+	WALL = '#',
+	EMPTY = '.',
+	OXY = 'O',
+	DROID = 'D',
+	UNKNOWN = '_'
+}
+
+export const statusToTile = (s: Status): Tile => {
+	switch (s) {
+		case Status.FINISHED:
+			return Tile.OXY;
+		case Status.MOVED:
+			return Tile.EMPTY;
+		case Status.WALL:
+			return Tile.WALL;
+	}
+};
+
+const W = 10;
+const H = 10;
+
+export const draw = (m: Map<string, string>): void => {
+	console.log(printMatrix(drawMapStatic(m, t => t ?? '_', -W, W, -H, H)));
+};
--- src/2019/day15/typescript/part_one.ts
+++ src/2019/day15/typescript/part_one.ts
@@ -1,79 +1,46 @@
 import { bench, read } from '@lib';
-import { drawMapStatic, printMatrix } from '@lib/functions';
 import { IntCodeComputer } from '@lib/intcode';
 import { Direction, Vec2 } from '@lib/model';
 import { day, year } from '.';
+import { GridGraph, GridGraphNode } from './graph.class';
+import { draw, getMove, Status, Tile } from './meta';
 import { parse } from './parse';
 
-export enum Move {
-	NORTH = 1,
-	SOUTH = 2,
-	WEST = 3,
-	EAST = 4
-}
-
-export const getDirection = (move: Move): Direction => {
-	switch (move) {
-		case Move.NORTH:
-			return Direction.NORTH;
-		case Move.SOUTH:
-			return Direction.SOUTH;
-		case Move.EAST:
-			return Direction.EAST;
-		case Move.WEST:
-			return Direction.WEST;
-		default:
-			return Direction.NONE;
-	}
-};
-
-export enum Status {
-	WALL = 0,
-	MOVED = 1,
-	FINISHED = 2
-}
-
-export enum Tile {
-	WALL = '#',
-	EMPTY = '.',
-	OXY = 'O',
-	DROID = 'D'
-}
-
-const W = 10;
-const H = 10;
-
-const draw = (m: Map<string, string>): void => {
-	console.log(printMatrix(drawMapStatic(m, t => t ?? '_', -W, W, -H, H)));
-};
-
-export const runner = async (input: string) => {
+export const runner = (print = false) => (input: string) => {
 	const i = new IntCodeComputer(parse(input));
 	const it = i.iter();
-	let s!: Status;
-	const p = new Vec2(0, 0);
+	const p = Vec2.ORIGO.clone();
 	const map = new Map<string, Tile>();
-	while (s !== Status.FINISHED) {
-		const nextMove = Move.WEST;
-		const dir = getDirection(nextMove);
+	map.set(Vec2.ORIGO.toString(), Tile.DROID);
+	const g = new GridGraph();
 
-		i.pushInput(nextMove);
-		s = it.next().value;
-		if (s === Status.WALL) {
-			map.set(p.toString(), Tile.WALL);
-		} else {
-			map.set(p.toString(), Tile.EMPTY);
-			p.addMut(dir);
-			map.set(p.toString(), s === Status.MOVED ? Tile.DROID : Tile.OXY);
-		}
+	const res = g.search(
+		[new GridGraphNode(Vec2.ORIGO.clone(), Tile.EMPTY)],
+		(dir: Direction) => {
+			i.pushInput(getMove(dir));
+			const s: Status = it.next().value;
+			if (s === Status.FINISHED) {
+				console.log('!!!!!!!!!!!!!!!!! FINISHED');
+			}
+			if (print) {
+				if (s === Status.WALL) {
+					map.set(p.add(dir).toString(), Tile.WALL);
+				} else {
+					map.set(p.toString(), Tile.EMPTY);
+					p.addMut(dir);
+					map.set(p.toString(), s === Status.MOVED ? Tile.DROID : Tile.OXY);
+				}
+				draw(map);
+			}
 
-		draw(map);
-	}
+			return s;
+		},
+		t => t === Tile.OXY
+	);
 
-	return 0;
+	return res?.pos.toString();
 };
 
 if (require.main === module) {
-	// (async () => console.log(`Result: ${await runner('')}`))();
-	(async () => console.log(`Result: ${await bench(read(year, day), runner)}`))(); // 0 ~0ms
+	(async () => console.log(`Result: ${await bench(read(year, day), runner(true))}`))(); // 0 ~0ms
 }
