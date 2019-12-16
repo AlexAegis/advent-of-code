e6f3193c8540c8d0d6f2e3b5965e92b9663c62e4
AlexAegis <alexaegis@gmail.com>
2019-12-16 23:11:35 +0100

    2019 16

--- src/2019/day16/typescript/part_two.ts
+++ src/2019/day16/typescript/part_two.ts
@@ -1,11 +1,23 @@
 import { bench, read } from '@lib';
 import { day, year } from '.';
+import { parse } from './parse';
+import { ftt } from './part_one';
 
-export const runner = async (input: string) => {
-	return 0;
+export const patternGet = (pattern: number[], forNum: number, phase: number) => {
+	return pattern[Math.floor(forNum / (phase + 1) + 1 / (phase + 1)) % pattern.length];
+};
+export const runner = (input: string) => {
+	const a = parse(input.repeat(1));
+	const messageOffset = parseInt(a.slice(0, 7).join(''), 10);
+	console.log(messageOffset);
+	const f = ftt(a, messageOffset);
+
+	return f;
 };
 
 if (require.main === module) {
-	(async () => console.log(`Result: ${await runner('')}`))();
-	// (async () => console.log(`Result: ${await bench(read(year, day), runner)}`))(); // 0 ~0ms
+	// (async () => console.log(`Result: ${await runner('12345678')}`))();
+	(async () => console.log(`Result: ${await runner('03036732577212944063491565474664')}`))();
+	// 	(async () => console.log(`Result: ${await runner('03036732577212944063491565474664')}`))();
+	// (async () => console.log(`Result: ${await bench(read(year, day), runner)}`))(); // 30369587 ~0ms
 }
