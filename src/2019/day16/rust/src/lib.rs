d83ad2ef649bcc7e7b81e838644fadc3acbef14d
AlexAegis <alexaegis@gmail.com>
2019-12-16 23:11:35 +0100

    2019 16

--- src/2019/day16/rust/src/lib.rs
+++ src/2019/day16/rust/src/lib.rs
@@ -1,14 +1,56 @@
 pub struct PartOne;
 pub struct PartTwo;
 
-impl aoclib::Solvable<&str, i32> for PartOne {
-	fn solve(input: &str) -> aoclib::Solution<i32> {
-		Ok(0)
+pub const PATTERN: [isize; 4] = [0, 1, 0, -1];
+
+fn pattern_get(n: usize, phase: usize) -> isize {
+	PATTERN[(n / (phase + 1) + 1 / (phase + 1)) % PATTERN.len()]
+}
+
+fn ftt(inp: &mut Vec<isize>, offset: usize) -> String {
+	let mut a: Vec<isize> = inp.to_vec();
+	println!("{:?}", inp);
+	println!("{:?}", a);
+	for _ in 0..100 {
+		let mut p: Vec<isize> = a.to_vec();
+		println!("{:?}", p);
+		for (r, _g) in a.iter().enumerate() {
+			let mut s: isize = 0;
+			for i in 0..a.len() {
+				s += (*a.get(i).unwrap() as isize * pattern_get(i, r)) % 10;
+			}
+			p[r] = (s % 10).abs();
+		}
+		a = p;
+	}
+
+	let from = offset % a.len();
+	let mut to = (8 + offset) % a.len();
+	while to <= from {
+		to += a.len();
+	}
+	println!("{:?}", a);
+	std::iter::repeat(a.iter())
+		.flatten()
+		.skip(from)
+		.take(8)
+		.map(|i| i.to_string())
+		.collect::<String>()
+}
+impl aoclib::Solvable<&str, String> for PartOne {
+	fn solve(input: &str) -> aoclib::Solution<String> {
+		let mut a = input
+			.repeat(1)
+			.chars()
+			.map(|c| c.to_string().parse::<isize>().unwrap())
+			.collect::<Vec<isize>>();
+
+		Ok(ftt(&mut a, 0))
 	}
 }
 
 impl aoclib::Solvable<&str, i32> for PartTwo {
-	fn solve(input: &str) -> aoclib::Solution<i32> {
+	fn solve(_input: &str) -> aoclib::Solution<i32> {
 		Ok(0)
 	}
 }
--- src/2019/day16/typescript/part_one.ts
+++ src/2019/day16/typescript/part_one.ts
@@ -1,13 +1,48 @@
 import { bench, read } from '@lib';
 import { day, year } from '.';
-import { parseLines } from './parse';
+import { parse } from './parse';
 
-export const runner = async (input: string) => {
-	const a = parseLines(input);
-	return 0;
+export const patternGet = (pattern: number[], forNum: number, phase: number) => {
+	return pattern[Math.floor(forNum / (phase + 1) + 1 / (phase + 1)) % pattern.length];
+};
+
+export const ftt = (a: number[], messageOffset: number = 0, multiplier = 1): string => {
+	const pattern = [0, 1, 0, -1];
+
+	const target = 100;
+	for (let pn = 0; pn < target; pn++) {
+		const p: number[] = [];
+		for (let r = 0; r < a.length * multiplier; r++) {
+			let s = 0;
+			for (let i = 0; i < a.length * multiplier; i += 1) {
+				s += (a[i % a.length] * patternGet(pattern, i, r)) % 10;
+			}
+			p[r] = Math.abs(s % 10);
+		}
+		a = p;
+
+		console.log(p);
+	}
+
+	const from = (0 + messageOffset) % a.length;
+	let to = (20 + messageOffset) % a.length;
+	while (to <= from) {
+		to += a.length;
+	}
+	const q = a.join('').match(/.{1,8}/g);
+	return q?.join(' ') ?? '';
+	return [...a, ...a].slice(from, to).join('');
+};
+
+export const runner = (input: string) => {
+	const a = parse(input);
+
+	return ftt(a, 0, 1);
 };
 
 if (require.main === module) {
-	(async () => console.log(`Result: ${await runner('')}`))();
-	// (async () => console.log(`Result: ${await bench(read(year, day), runner)}`))(); // 0 ~0ms
+	(async () => console.log(`Result: ${await runner('12345678')}`))();
+	// (async () => console.log(`Result: ${await runner('80871224585914546619083218645595')}`))();
+	// (async () => console.log(`Result: ${await runner('03036732577212944063491565474664')}`))();
+	(async () => console.log(`Result: ${await bench(read(year, day), runner)}`))(); // 30369587 ~0ms
 }
