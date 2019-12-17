e339eb7a8b69487c44fa4e484d558fa49465adca
AlexAegis <alexaegis@gmail.com>
2019-12-17 05:51:04 +0100

    exp

--- src/2019/day16/typescript/part_two.ts
+++ src/2019/day16/typescript/part_two.ts
@@ -1,23 +1,55 @@
 import { bench, read } from '@lib';
+import { lcm } from '@lib/functions';
 import { day, year } from '.';
 import { parse } from './parse';
-import { ftt } from './part_one';
 
-export const patternGet = (pattern: number[], forNum: number, phase: number) => {
-	return pattern[Math.floor(forNum / (phase + 1) + 1 / (phase + 1)) % pattern.length];
+export const pattern = [0, 1, 0, -1] as const;
+
+export const patternGet = (n: number, p: number) => {
+	return pattern[Math.floor(n / p + 1 / p) % pattern.length];
 };
+
+export const phaser = (a: number[], c: number = 1, f: (a: number[], phase: number) => number[]): number[] => {
+	let p!: number[];
+	for (let phase = 0; phase < c; phase++) {
+		p = f(p || a, phase);
+	}
+	return p;
+};
+
+export const fft = (m: number = 1) => (a: number[], _phase: number): number[] => {
+	const p = [];
+
+	const alen = a.length;
+	const plen = pattern.length;
+	const prowlen = lcm(alen, m * plen);
+	console.log('prowlen', prowlen);
+
+	for (let row = 1; row <= a.length * m; row++) {
+		let s = 0;
+		for (let col = 0; col < prowlen; col += 1) {
+			// In the case of the first number, its just multiplied by the multiplier
+			s += (a[col % a.length] * patternGet(col, row)) % 10;
+			// idea is to SUM until LCM of (alen and plen * row) prowlen
+			// then this can
+		}
+		s *= m;
+		p[row - 1] = Math.abs(s % 10);
+	}
+
+	return p;
+};
+
 export const runner = (input: string) => {
 	const a = parse(input.repeat(1));
-	const messageOffset = parseInt(a.slice(0, 7).join(''), 10);
-	console.log(messageOffset);
-	const f = ftt(a, messageOffset);
-
-	return f;
+	return phaser(a, 1, fft(1))
+		.slice(0, 8)
+		.join('');
 };
 
 if (require.main === module) {
-	// (async () => console.log(`Result: ${await runner('12345678')}`))();
-	(async () => console.log(`Result: ${await runner('03036732577212944063491565474664')}`))();
+	(async () => console.log(`Result: ${await runner('12345678')}`))();
+	// (async () => console.log(`Result: ${await runner('03036732577212944063491565474664')}`))();
 	// 	(async () => console.log(`Result: ${await runner('03036732577212944063491565474664')}`))();
 	// (async () => console.log(`Result: ${await bench(read(year, day), runner)}`))(); // 30369587 ~0ms
 }
