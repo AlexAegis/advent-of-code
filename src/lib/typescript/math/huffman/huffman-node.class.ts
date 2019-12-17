07380b83238f4dcc9af08931388467f3bbc62ea7
AlexAegis <alexaegis@gmail.com>
2019-12-17 19:36:53 +0100

    huffmann

---
+++ src/lib/typescript/math/huffman/huffman-node.class.ts
@@ -0,0 +1,33 @@
+export class HuffmannNode {
+	public left?: HuffmannNode;
+	public right?: HuffmannNode;
+
+	public constructor(
+		public frequency: number,
+
+		public code?: string
+	) {}
+
+	public toString(prefix: string = ''): string {
+		let s = `${prefix}${this.code ?? '('}:${this.frequency}\n`;
+		if (this.left) {
+			s += this.left.toString(prefix + '\t');
+		}
+		if (this.right) {
+			s += this.right.toString(prefix + '\t');
+		}
+		return s;
+	}
+
+	public *codeTable(prefix: string = ''): IterableIterator<[string, string]> {
+		if (this.left) {
+			yield* this.left.codeTable(prefix + '1');
+		}
+		if (this.code) {
+			yield [this.code, prefix];
+		}
+		if (this.right) {
+			yield* this.right.codeTable(prefix + '0');
+		}
+	}
+}
