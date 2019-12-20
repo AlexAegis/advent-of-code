eff8d2511084f6bd7c0f958947e3eccc47818e6d
AlexAegis <alexaegis@gmail.com>
2019-12-20 05:50:28 +0100

    scaffold 2019 20

---
+++ src/2019/day20/rust/tests/part_one.rs
@@ -0,0 +1,14 @@
+use aoclib::Solvable;
+
+#[test]
+fn input() -> aoclib::Result<()> {
+	let input = aoclib::reader(2019, 20, "input.txt")?;
+	assert_eq!(aoc201920::PartOne::solve(&input)?, 0);
+	Ok(())
+}
+
+#[test]
+fn example_1() -> aoclib::Result<()> {
+	assert_eq!(aoc201920::PartOne::solve("")?, 0);
+	Ok(())
+}
