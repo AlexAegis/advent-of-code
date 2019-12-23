086a05d1dc17c0e3511ff154bb6167c37b0bb22d
AlexAegis <alexaegis@gmail.com>
2019-12-23 12:44:51 +0100

    shortened

--- src/2019/day23/typescript/part_one.ts
+++ src/2019/day23/typescript/part_one.ts
@@ -11,69 +11,81 @@ export class Packet {
 
 export const nullPacket = new Packet(-1, -1, -1);
 
+export const setupNetwork = (
+	tape: number[],
+	nat: (nat: Packet) => void
+): { network: Map<number, [IntCodeComputer, IterableIterator<number | undefined>]>; io: Map<number, Packet[]> } => {
+	const network = new Map<number, [IntCodeComputer, IterableIterator<number | undefined>]>();
+	const io = new Map<number, Packet[]>();
+	// setup
+	for (let i = 0; i < RC; i++) {
+		const nic = new IntCodeComputer(tape);
+		nic.pushInput(i);
+		nic.input = (() => {
+			let packet: undefined | Packet;
+			let readCount = 0;
+			return () => {
+				if (packet === undefined) {
+					packet = io.get(i)?.shift() ?? nullPacket;
+					readCount = 0;
+				}
+
+				let result: number;
+				if (readCount === 0) {
+					result = packet.x;
+				} else {
+					result = packet.y;
+					packet = undefined;
+				}
+
+				readCount++;
+				return result;
+			};
+		})();
+
+		nic.outputCallback = (() => {
+			let packet: undefined | Packet;
+			let readCount = 0;
+			return (out: number) => {
+				if (packet === undefined) {
+					packet = new Packet(-1, -1, -1);
+					readCount = 0;
+				}
+
+				if (readCount === 0) {
+					packet.destination = out;
+				} else if (readCount === 1) {
+					packet.x = out;
+				} else {
+					packet.y = out;
+					if (packet.destination === 255) {
+						nat(packet);
+					} else {
+						io.get(packet.destination)?.push(packet);
+					}
+					packet = undefined;
+				}
+				readCount++;
+				return out;
+			};
+		})();
+
+		network.set(i, [nic, nic.stepper()]);
+		io.set(i, []);
+	}
+
+	return { network, io };
+};
+
 export const runner = (input: string) =>
 	new Promise<number>(resolve => {
 		const tape = parse(input);
-		const network = new Map<number, [IntCodeComputer, IterableIterator<number | undefined>]>();
-		const io = new Map<number, Packet[]>();
 		let resolved = false;
-		// setup
-		for (let i = 0; i < RC; i++) {
-			const nic = new IntCodeComputer(tape);
-			nic.pushInput(i);
-			nic.input = (() => {
-				let packet: undefined | Packet;
-				let readCount = 0;
-				return () => {
-					if (packet === undefined) {
-						packet = io.get(i)?.shift() ?? nullPacket;
-						readCount = 0;
-					}
 
-					let result: number;
-					if (readCount === 0) {
-						result = packet.x;
-					} else {
-						result = packet.y;
-						packet = undefined;
-					}
-
-					readCount++;
-					return result;
-				};
-			})();
-
-			nic.outputCallback = (() => {
-				let packet: undefined | Packet;
-				let readCount = 0;
-				return (out: number) => {
-					if (packet === undefined) {
-						packet = new Packet(-1, -1, -1);
-						readCount = 0;
-					}
-
-					if (readCount === 0) {
-						packet.destination = out;
-					} else if (readCount === 1) {
-						packet.x = out;
-					} else {
-						packet.y = out;
-						if (packet.destination === 255) {
-							resolve(out);
-							resolved = true;
-						} else {
-							io.get(packet.destination)?.push(packet);
-						}
-						packet = undefined;
-					}
-					readCount++;
-					return out;
-				};
-			})();
-
-			network.set(i, [nic, nic.stepper()]);
-			io.set(i, []);
-		}
+		const { network } = setupNetwork(tape, (nat: Packet) => {
+			resolved = true;
+			resolve(nat.y);
+		});
 
 		while (!resolved) {
 			for (const [, [, stepper]] of network.entries()) {
