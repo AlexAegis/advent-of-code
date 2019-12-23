d1737012e037a5a00441cee72781ffd515cf3bac
AlexAegis <alexaegis@gmail.com>
2019-12-23 02:56:23 +0100

    bigint

---
+++ src/2019/day22/typescript/tests/part_two.spec.ts
@@ -0,0 +1,14 @@
+import { read } from '@lib';
+import { expect } from 'chai';
+import { day, results, year } from '..';
+import { runner } from '../part_two';
+
+describe(`${year} - Day ${day} - Part Two`, () => {
+	it(`should resolve to ${results.two.input} when using the input`, async () => {
+		expect(await runner((await read(year, day)()).input)).to.equal(results.two.input);
+	});
+
+	it('should be that that the first example resolves to 0', async () => {
+		expect(await runner('0')).to.equal(0);
+	});
+});
--- src/lib/typescript/functions/rotate-array.function.ts
+++ src/lib/typescript/functions/rotate-array.function.ts
@@ -1,3 +1,17 @@
+export const rotateArrayTimes = <T>(array: T[], n: number): T[] => {
+	let i = 0;
+	const na = Math.abs(n);
+	while (i < na) {
+		if (n > 0) {
+			array.push(array.shift() as any);
+		} else {
+			array.unshift(array.pop() as any);
+		}
+		i++;
+	}
+	return array;
+};
+
 export const rotateArray = <T>(array: T[], until: (t: T[], c?: T) => boolean): T[] => {
 	while (until(array, array[0])) {
 		array.push(array.shift() as any);
