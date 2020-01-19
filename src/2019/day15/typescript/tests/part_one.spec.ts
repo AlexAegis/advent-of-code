f828c7475fc0a69b0e6b58a0041447781f099582
AlexAegis <alexaegis@gmail.com>
2020-01-19 15:12:55 +0100

    fix

--- src/2019/day15/typescript/tests/part_one.spec.ts
+++ src/2019/day15/typescript/tests/part_one.spec.ts
@@ -5,10 +5,10 @@ import { runner } from '../part_one';
 
 describe(`${year} - Day ${day} - Part One`, () => {
 	it(`should resolve to ${results.one.input} when using the input`, async () => {
-		expect(await runner((await read(year, day)()).input)).to.equal(results.one.input);
+		expect(await runner()((await read(year, day)()).input)).to.equal(results.one.input);
 	});
 
 	it('should be that that both the first examples resolves to 0', async () => {
-		expect(await runner('')).to.equal(0);
+		expect(await runner()('')).to.equal(0);
 	});
 });
