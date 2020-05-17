14a353b20644c3e4e0f716fb108b6b3bc97e0f08
AlexAegis <alexaegis@gmail.com>
2020-05-17 16:40:42 +0200

    fmt

--- src/2019/day10/rust/src/main.rs
+++ src/2019/day10/rust/src/main.rs
@@ -1,5 +1,5 @@
-use aoclib::Solvable;
 use aoc201910::{PartOne, PartTwo};
+use aoclib::Solvable;
 
 pub fn main() -> aoclib::Result<()> {
 	let input = aoclib::reader(2019, 10, "input.txt")?;
--- src/2019/day23/rust/src/main.rs
+++ src/2019/day23/rust/src/main.rs
@@ -1,5 +1,5 @@
-use aoclib::Solvable;
 use aoc201923::{PartOne, PartTwo};
+use aoclib::Solvable;
 
 pub fn main() -> aoclib::Result<()> {
 	let input = aoclib::reader(2019, 23, "input.txt")?;
