04083eec2c7853222c6867739c3e298e51c13ef8
AlexAegis <alexaegis@gmail.com>
2020-05-17 18:13:35 +0200

    timeout

--- src/2019/day16/typescript/tests/part_one.spec.ts
+++ src/2019/day16/typescript/tests/part_one.spec.ts
@@ -6,5 +6,5 @@ import { runner } from '../part_one';
 describe(`${year} - Day ${day} - Part One`, () => {
 	it(`should resolve to ${results.one.input} when using the input`, async () => {
 		expect(await runner((await read(year, day)()).input)).to.equal(results.one.input);
-	});
+	}).timeout(20000);
 });
