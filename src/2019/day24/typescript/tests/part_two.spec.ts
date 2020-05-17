c4a0c7c443e278941cd9f4697cfb772f5b1d5e27
AlexAegis <alexaegis@gmail.com>
2020-05-17 18:24:09 +0200

    no macos

--- src/2019/day24/typescript/tests/part_two.spec.ts
+++ src/2019/day24/typescript/tests/part_two.spec.ts
@@ -6,7 +6,7 @@ import { runner } from '../part_two';
 describe(`${year} - Day ${day} - Part Two`, () => {
 	it(`should resolve to ${results.two.input} when using the input`, async () => {
 		expect(await runner()((await read(year, day)()).input)).to.equal(results.two.input);
-	});
+	}).timeout(20000);
 
 	it(`should resolve to 99 when using the first example`, async () => {
 		expect(await runner(10)((await read(year, day, 'example.1.txt')()).input)).to.equal(99);
