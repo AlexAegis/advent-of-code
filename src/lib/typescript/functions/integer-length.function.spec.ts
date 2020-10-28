7c4b54c3cd684ea0c32e42cc517c38afbc1ab42c
AlexAegis <alexaegis@gmail.com>
2020-10-28 19:52:34 +0100

    fix: integer length

---
+++ src/lib/typescript/functions/integer-length.function.spec.ts
@@ -0,0 +1,43 @@
+import { expect } from 'chai';
+import { integerLength, integerLengthMath } from './integer-length.function';
+
+describe(`Integer length`, () => {
+	it(`should work with numbers larger than 1`, async () => {
+		expect(integerLength(123)).to.equal(3);
+		expect(integerLength(7653)).to.equal(4);
+		expect(integerLength(85476847)).to.equal(8);
+	});
+
+	it(`should work with 1`, async () => {
+		expect(integerLength(1)).to.equal(1);
+	});
+
+	it(`should work with 0`, async () => {
+		expect(integerLength(0)).to.equal(1);
+	});
+
+	it(`should work with negative numbers`, async () => {
+		expect(integerLength(-34312)).to.equal(5);
+	});
+});
+
+// tslint:disable: deprecation
+describe(`Integer length (Math)`, () => {
+	it(`should work with numbers larger than 1`, async () => {
+		expect(integerLengthMath(123)).to.equal(3);
+		expect(integerLengthMath(7653)).to.equal(4);
+		expect(integerLengthMath(85476847)).to.equal(8);
+	});
+
+	it(`should work with 1`, async () => {
+		expect(integerLengthMath(1)).to.equal(1);
+	});
+
+	it(`should work with 0`, async () => {
+		expect(integerLengthMath(0)).to.equal(0);
+	});
+
+	it(`should work with negative numbers`, async () => {
+		expect(integerLengthMath(-34312)).to.equal(5);
+	});
+});
