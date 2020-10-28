c7a036ca4abe19d0b12494f2eda851fbb5d8135c
AlexAegis <alexaegis@gmail.com>
2020-10-28 21:59:46 +0100

    chore: static test descriptions

--- src/2019/day05/typescript/tests/part_two.spec.ts
+++ src/2019/day05/typescript/tests/part_two.spec.ts
@@ -5,7 +5,7 @@ import { day, results, year } from '..';
 import { parse } from '../parse';
 import { runner } from '../part_two';
 
-describe(`${year} - Day ${day} - Part Two`, () => {
+describe(`2019 - Day 5 - Part Two`, () => {
 	const seventhInput = parse(
 		'3,21,1008,21,8,20,1005,20,22,107,8,21,20,1006,20,31,1106,0,36,98,0,0,1002,21,125,20,4,20,1105,1,46,104,999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99'
 	);
--- src/2019/day11/typescript/tests/part_two.spec.ts
+++ src/2019/day11/typescript/tests/part_two.spec.ts
@@ -3,7 +3,7 @@
 import { day, results, year } from '..';
 // import { runner } from '../part_two';
 
-describe(`${year} - Day ${day} - Part Two`, () => {
+describe(`2019 - Day 11 - Part Two`, () => {
 	// TODO: ASCII ART PARSER
 	it(`should resolve to ${results.two.input} when using the input`, async () => {
 		// expect(await runner((await read(year, day)()).input)).to.equal(results.two.input);
