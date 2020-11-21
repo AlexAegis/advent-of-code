093c40a6c8c1ff64bf9be905d87389eaa44cbdba
AlexAegis <alexaegis@gmail.com>
2020-11-21 19:30:26 +0100

    chore: fix clippy

--- src/2019/day16/rust/src/lib.rs
+++ src/2019/day16/rust/src/lib.rs
@@ -41,6 +41,7 @@ impl aoclib::Solvable<&str, String> for PartOne {
 	fn solve(input: &str) -> aoclib::Solution<String> {
 		let mut a = input
 			.to_string()
+			.clone()
 			.repeat(1)
 			.chars()
 			.map(|c| c.to_string().parse::<isize>().unwrap())
