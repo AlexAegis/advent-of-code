d06e2253ae94440acda7cc82bb622797a18b7b25
AlexAegis <alexaegis@gmail.com>
2020-11-21 19:39:56 +0100

    chore: fix clippy issue

--- src/2019/day16/rust/src/lib.rs
+++ src/2019/day16/rust/src/lib.rs
@@ -39,10 +39,7 @@ fn ftt(inp: &mut Vec<isize>, offset: usize) -> String {
 }
 impl aoclib::Solvable<&str, String> for PartOne {
 	fn solve(input: &str) -> aoclib::Solution<String> {
-		let mut a = input
-			.to_string()
-			.clone()
-			.repeat(1)
+		let mut a = input.to_string()
 			.chars()
 			.map(|c| c.to_string().parse::<isize>().unwrap())
 			.collect::<Vec<isize>>();
