66b0fc88569679571bcc7de6f0d338c56ff19c7b
AlexAegis <alexaegis@gmail.com>
2020-11-30 09:04:11 +0100

    feat(typescript): 2016 day 2 part 2 solved

--- src/2016/day02/typescript/tests/part_two.spec.ts
+++ src/2016/day02/typescript/tests/part_two.spec.ts
@@ -7,4 +7,8 @@ describe(`2016 - Day 2 - Part Two`, () => {
 	it(`should resolve to ${results.two.input} when using the input`, async () => {
 		expect(await runner((await read(year, day)()).input)).to.equal(results.two.input);
 	});
+
+	it('should be that that the first example resolves to 5DB3', async () => {
+		expect(await runner((await read(year, day, 'example.1.txt')()).input)).to.equal('5DB3');
+	});
 });
