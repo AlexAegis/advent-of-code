1154de44ae836549aa14353743bb6f205c1a271d
AlexAegis <alexaegis@gmail.com>
2020-11-30 07:06:13 +0100

    chore(typescript): rename ORIGIN

--- src/2016/day02/typescript/part_two.ts
+++ src/2016/day02/typescript/part_two.ts
@@ -4,7 +4,11 @@ import { Vec2 } from '@lib/model/vec2.class';
 import { day, year } from '.';
 
 export const runner = (input: string): number => {
-	const acc = { position: Vec2.ORIGO, direction: Direction.NORTH, history: new Set<string>() };
+	const acc = {
+		position: Vec2.ORIGIN.clone(),
+		direction: Direction.NORTH,
+		history: new Set<string>(),
+	};
 	loop: for (const next of input.split(', ')) {
 		if (next[0] === 'R') acc.direction = acc.direction.right();
 		if (next[0] === 'L') acc.direction = acc.direction.left();
@@ -15,7 +19,7 @@ export const runner = (input: string): number => {
 			acc.history.add(coordString);
 		}
 	}
-	return acc.position.manhattan(Vec2.ORIGO);
+	return acc.position.manhattan(Vec2.ORIGIN);
 };
 
 if (require.main === module) {
