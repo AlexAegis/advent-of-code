1d54e29a7477cf094898c27ad31ee9bed2de700e
AlexAegis <alexaegis@gmail.com>
2020-12-02 06:47:45 +0100

    feat(typescript): 2020 day 2 prettify solutions

--- src/2020/day02/typescript/part_two.ts
+++ src/2020/day02/typescript/part_two.ts
@@ -1,26 +1,16 @@
 import { bench, read, split } from '@lib';
 import { day, year } from '.';
+import { parseLine } from './part_one';
 
-export const runner = (input: string): number => {
-	let count = 0;
-	for (const line of split(input)) {
-		const [name, val] = line.split(': ');
-		const [nums, letter] = name.split(' ');
-		const [numa, numb] = nums.split('-');
-		const na = parseInt(numa, 10);
-		const nb = parseInt(numb, 10);
-
-		const aa = val[na - 1] === letter;
-		const ab = val[nb - 1] === letter;
-
-		if (aa !== ab) {
-			count = count + 1;
-		}
-	}
-
-	return count;
-};
+export const runner = (input: string): number =>
+	split(input)
+		.map(parseLine)
+		.filter(({ low, high, letter, password }) => {
+			const lowMatch = password[low - 1] === letter;
+			const highMatch = password[high - 1] === letter;
+			return lowMatch !== highMatch;
+		}).length;
 
 if (require.main === module) {
-	(async () => console.log(`Result: ${await bench(read(year, day), runner)}`))(); // 593 ~2.4ms
+	(async () => console.log(`Result: ${await bench(read(year, day), runner)}`))(); // 593 ~2ms
 }
