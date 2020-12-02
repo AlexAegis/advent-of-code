a4b7dfd00a25e07daa033c82ca9402e953417086
AlexAegis <alexaegis@gmail.com>
2020-12-02 06:47:45 +0100

    feat(typescript): 2020 day 2 prettify solutions

--- src/2020/day02/typescript/part_one.ts
+++ src/2020/day02/typescript/part_one.ts
@@ -1,25 +1,30 @@
 import { bench, read, split } from '@lib';
 import { day, year } from '.';
 
-export const runner = (input: string): number => {
-	let count = 0;
-	for (const line of split(input)) {
-		const [name, val] = line.split(': ');
-		const [nums, letter] = name.split(' ');
-		const [numa, numb] = nums.split('-');
-		const na = parseInt(numa, 10);
-		const nb = parseInt(numb, 10);
+export const lineMatcher = /(\d+)-(\d+) ([a-z]): ([a-z]+)/;
 
-		const c = val.split(letter).length - 1;
-		if (c <= nb && c >= na) {
-			count = count + 1;
-		}
-	}
+export interface TobogganPasswordPolicy {
+	low: number;
+	high: number;
+	letter: string;
+	password: string;
+}
 
-	return count;
+export const parseLine = (line: string): TobogganPasswordPolicy => {
+	const [, lows, highs, letter, password] = line.match(lineMatcher) ?? [];
+	const low = parseInt(lows, 10);
+	const high = parseInt(highs, 10);
+	return { low, high, letter, password };
 };
 
+export const runner = (input: string): number =>
+	split(input)
+		.map(parseLine)
+		.filter(({ low, high, letter, password }) => {
+			const characterCount = password.split(letter).length - 1;
+			return characterCount >= low && characterCount <= high;
+		}).length;
+
 if (require.main === module) {
-	(async () => console.log(`Result: ${await bench(read(year, day), runner)}`))(); // 493 ~3.7ms
-	// (async () => console.log(`Result: ${await bench(read(year, day, 'example.1.txt'), runner)}`))(); // 787776 ~0.37ms
+	(async () => console.log(`Result: ${await bench(read(year, day), runner)}`))(); // 493 ~2.6ms
 }
