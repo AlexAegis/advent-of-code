81e11fc89374151e6e71c85360dd7ea9083c51b0
AlexAegis <alexaegis@gmail.com>
2020-12-02 05:59:53 +0100

    feat(typescript): 2020 day2 prepare

---
+++ src/2020/day02/typescript/index.ts
@@ -0,0 +1,13 @@
+import { DayResults } from '@lib';
+
+export const year = 2020;
+export const day = 2;
+
+export const results: DayResults = {
+	one: {
+		input: 787776,
+	},
+	two: {
+		input: 262738554,
+	},
+};
