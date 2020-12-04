8ba848dbe752262dec7e3c211abcec2739a14819
AlexAegis <alexaegis@gmail.com>
2020-12-04 12:54:03 +0100

    chore(typescript): added second input

--- src/2020/day04/typescript/tests/part_one.spec.ts
+++ src/2020/day04/typescript/tests/part_one.spec.ts
@@ -11,4 +11,8 @@ describe(`2020 - Day 4 - Part One`, () => {
 	it('should solve for the first example', async () => {
 		expect(await runner((await read(year, day, 'example.1.txt')()).input)).to.equal(2);
 	});
+
+	it('should solve for the second input', async () => {
+		expect(await runner((await read(year, day, 'input.2.txt')()).input)).to.equal(256);
+	});
 });
