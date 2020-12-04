45d27e56a79d2c1d95e436ec484a2cd72ba5846d
AlexAegis <alexaegis@gmail.com>
2020-12-04 12:54:03 +0100

    chore(typescript): added second input

--- src/2020/day04/typescript/tests/part_two.spec.ts
+++ src/2020/day04/typescript/tests/part_two.spec.ts
@@ -5,19 +5,23 @@ import { passportChecks, runner } from '../part_two';
 
 describe(`2020 - Day 4 - Part Two`, () => {
 	it(`should solve the input`, async () => {
-		expect(await runner((await read(year, day)()).input)).to.equal(224);
+		expect(runner((await read(year, day)()).input)).to.equal(224);
 	});
 
 	it('should solve the first example', async () => {
-		expect(await runner((await read(year, day, 'example.1.txt')()).input)).to.equal(2);
+		expect(runner((await read(year, day, 'example.1.txt')()).input)).to.equal(2);
 	});
 
 	it('should solve the all valid example', async () => {
-		expect(await runner((await read(year, day, 'example.valid.txt')()).input)).to.equal(4);
+		expect(runner((await read(year, day, 'example.valid.txt')()).input)).to.equal(4);
 	});
 
 	it('should solve the all invalid example', async () => {
-		expect(await runner((await read(year, day, 'example.invalid.txt')()).input)).to.equal(0);
+		expect(runner((await read(year, day, 'example.invalid.txt')()).input)).to.equal(0);
+	});
+
+	it('should solve for the second input', async () => {
+		expect(runner((await read(year, day, 'input.2.txt')()).input)).to.equal(198);
 	});
 
 	it('should have a working byr check', async () => {
