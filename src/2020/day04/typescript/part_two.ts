045a93a862923288205626be194b2a5419879e04
AlexAegis <alexaegis@gmail.com>
2020-12-04 11:03:01 +0100

    chore(typescript): remove unnecessary types

--- src/2020/day04/typescript/part_two.ts
+++ src/2020/day04/typescript/part_two.ts
@@ -8,10 +8,10 @@ const IN = 'in';
 export const passportEyeColors = ['amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth'];
 
 export const passportChecks: Record<RelevantField, (v: string) => boolean> = {
-	byr: (byr: string): boolean => byr.toInt()?.isBetween(1920, 2002) ?? false,
-	iyr: (iyr: string): boolean => iyr.toInt()?.isBetween(2010, 2020) ?? false,
-	eyr: (eyr: string): boolean => eyr.toInt()?.isBetween(2020, 2030) ?? false,
-	hgt: (hgt: string): boolean => {
+	byr: (byr) => byr.toInt()?.isBetween(1920, 2002) ?? false,
+	iyr: (iyr) => iyr.toInt()?.isBetween(2010, 2020) ?? false,
+	eyr: (eyr) => eyr.toInt()?.isBetween(2020, 2030) ?? false,
+	hgt: (hgt) => {
 		if (hgt.endsWith(CM)) {
 			return hgt.split(CM)[0].toInt()?.isBetween(150, 193) ?? false;
 		} else if (hgt.endsWith(IN)) {
@@ -20,9 +20,9 @@ export const passportChecks: Record<RelevantField, (v: string) => boolean> = {
 			return false;
 		}
 	},
-	hcl: (hcl: string): boolean => /^#[0-9a-f]{6}$/.test(hcl),
-	ecl: (ecl: string): boolean => !!passportEyeColors.find((color) => color === ecl),
-	pid: (pid: string): boolean => /^[0-9]{9}$/.test(pid),
+	hcl: (hcl) => /^#[0-9a-f]{6}$/.test(hcl),
+	ecl: (ecl) => !!passportEyeColors.find((color) => color === ecl),
+	pid: (pid) => /^[0-9]{9}$/.test(pid),
 };
 
 export const isValidPassport = (passport: Partial<Passport>): passport is Passport =>
