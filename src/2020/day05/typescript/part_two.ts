48d81b6a4a280df4a091f812d0644012ff8c7fe9
AlexAegis <alexaegis@gmail.com>
2020-12-05 08:57:19 +0100

    feat(typescript): 2020 day 5 solved

---
+++ src/2020/day05/typescript/part_two.ts
@@ -0,0 +1,18 @@
+import { bench, read, split } from '@lib';
+import { day, year } from '.';
+import { calculateSeatId } from './part_one';
+
+export const runner = (input: string): number => {
+	const seats = [];
+	for (const seatId of split(input).map(calculateSeatId)) {
+		seats[seatId] = seatId;
+	}
+	while (seats[0] === undefined) {
+		seats.shift();
+	}
+	return seats.findIndex((seat) => seat === undefined) + seats[0];
+};
+
+if (require.main === module) {
+	(async () => console.log(`Result: ${await bench(read(year, day), runner)}`))(); // 682 ~4ms
+}
