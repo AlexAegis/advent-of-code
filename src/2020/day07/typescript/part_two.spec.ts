69b6ce89134787d1c1b2e3f6e3f32011fbb6c9a6
AlexAegis <alexaegis@gmail.com>
2020-12-07 07:45:04 +0100

    feat(typescript): 2020 day 7 solved

---
+++ src/2020/day07/typescript/part_two.spec.ts
@@ -0,0 +1,18 @@
+import { read } from '@lib';
+import { expect } from 'chai';
+import { day, year } from '.';
+import { runner } from './part_two';
+
+describe(`2020 - Day 7 - Part Two`, () => {
+	it(`should solve the input`, async () => {
+		expect(runner((await read(year, day)()).input)).to.equal(85324);
+	});
+
+	it('should solve the first example', async () => {
+		expect(runner((await read(year, day, 'example.1.txt')()).input)).to.equal(32);
+	});
+
+	it('should solve the second example', async () => {
+		expect(runner((await read(year, day, 'example.2.txt')()).input)).to.equal(126);
+	});
+});
---
+++ src/2020/day07/typescript/part_two.ts
@@ -0,0 +1,46 @@
+import { bench, read, split } from '@lib';
+import { day, year } from '.';
+import { Bag } from './bag.class';
+
+export const runner = (input: string): number => {
+	const lines = split(input);
+
+	const bags = new Map<string, Bag>();
+
+	const getBag = (color: string): Bag => {
+		if (!bags.has(color)) {
+			bags.set(color, new Bag(color));
+		}
+		return bags.get(color) as Bag;
+	};
+	for (const line of lines) {
+		//const matches = line.match(
+		//	/^([a-z ]+) bags contain ((, )?((\d+) (\w+) bags)|(no other bags))*.$/
+		//);
+
+		const [color, toRaw] = line.split(' bags contain ');
+
+		const toRules = toRaw
+			.split(', ')
+			.map((toRuleRaw) => toRuleRaw.replace('.', '').replace(/bags?/g, '').trim().split(' '))
+			.map(([q, ...rest]) => ({
+				c: rest.join(' '),
+				q: parseInt(q, 10),
+			}));
+
+		const bag = getBag(color);
+		for (const rule of toRules) {
+			if (!isNaN(rule.q)) {
+				const bagRule = getBag(rule.c);
+				bag.canContain.set(bagRule, rule.q);
+			}
+		}
+	}
+
+	const goldBag = getBag('shiny gold');
+	return goldBag.howManyBagsCanItContain();
+};
+
+if (require.main === module) {
+	(async () => console.log(`Result: ${await bench(read(year, day), runner)}`))(); // 85324 ~3.9ms
+}
