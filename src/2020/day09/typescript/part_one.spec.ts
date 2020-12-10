24f5b27e93f31d4e6b5161f44907734a834ef870
AlexAegis <alexaegis@gmail.com>
2020-12-10 09:33:39 +0100

    feat(typescript): 2020 day 9 solved

---
+++ src/2020/day09/typescript/part_one.spec.ts
@@ -0,0 +1,30 @@
+import { read } from '@lib';
+import { expect } from 'chai';
+import { Args, day, year } from '.';
+import { hasComponents, runner } from './part_one';
+
+describe(`2020 - Day 9 - Part One`, () => {
+	const preamble = Array.from({ length: 25 }, (_, i) => i + 1);
+
+	it(`should solve for the input`, async () => {
+		const { input, args } = await read<Args>(year, day)();
+		expect(await runner(input, args)).to.equal(217430975);
+	});
+
+	it('should find a pair in the preamble that adds to 26', () =>
+		expect(hasComponents(26, preamble)).to.be.true);
+
+	it('should find a pair in the preamble that adds to 49', () =>
+		expect(hasComponents(49, preamble)).to.be.true);
+
+	it('should not find a pair in the preamble that adds to 100', () =>
+		expect(hasComponents(100, preamble)).to.be.false);
+
+	it('should not find a pair in the preamble that adds to 50', () =>
+		expect(hasComponents(50, preamble)).to.be.false);
+
+	it('should solve for the first example', async () => {
+		const { input, args } = await read<Args>(year, day, 'example.1.txt')();
+		expect(await runner(input, args)).to.equal(127);
+	});
+});
