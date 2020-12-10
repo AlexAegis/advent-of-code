e91bd36ebfc9c91380b8bccfdbb5cecb6585fe9a
AlexAegis <alexaegis@gmail.com>
2020-12-10 09:34:56 +0100

    feat(typescript): 2020 day 10 solved

---
+++ src/2020/day10/typescript/part_one.ts
@@ -0,0 +1,24 @@
+import { bench, read } from '@lib';
+import { day, year } from '.';
+import { parse } from './parse.function';
+
+export const runner = (input: string): number => {
+	const { jolts, max } = parse(input);
+
+	const diffs = new Map<number, number>();
+	let currentJoltage = 0;
+
+	while (currentJoltage <= max) {
+		const next =
+			jolts.find((jolt) => jolt - currentJoltage <= 3 && jolt - currentJoltage > 0) ?? 0;
+		const diff = next - currentJoltage;
+		currentJoltage = next;
+		diffs.set(diff, (diffs.get(diff) ?? 0) + 1);
+	}
+
+	return (diffs.get(1) ?? 0) * (diffs.get(3) ?? 0);
+};
+
+if (require.main === module) {
+	(async () => console.log(`Result: ${await bench(read(year, day), runner)}`))(); // 2201 ~0.4ms
+}
---
+++ src/2020/day10/typescript/part_two.ts
@@ -0,0 +1,38 @@
+import { bench, read } from '@lib';
+import { day, year } from '.';
+import { parse } from './parse.function';
+
+const countBranches = (
+	jolts: number[],
+	index: number,
+	builtIn: number,
+	cache: Map<number, number> = new Map<number, number>()
+): number => {
+	const current = jolts[index];
+	if (current === builtIn) {
+		return 1;
+	}
+	let result = 0;
+	for (let i = 1; i <= 3; i++) {
+		const next = jolts[index + i];
+		if (next - current <= 3) {
+			if (cache.has(index + i)) {
+				result += cache.get(index + i) ?? 0;
+			} else {
+				const branchResult = countBranches(jolts, index + i, builtIn, cache);
+				cache.set(index + i, branchResult);
+				result += branchResult;
+			}
+		}
+	}
+	return result;
+};
+
+export const runner = (input: string): number => {
+	const { jolts, builtIn } = parse(input);
+	return countBranches(jolts, 0, builtIn);
+};
+
+if (require.main === module) {
+	(async () => console.log(`Result: ${await bench(read(year, day), runner)}`))(); // 169255295254528 ~0.3ms
+}
