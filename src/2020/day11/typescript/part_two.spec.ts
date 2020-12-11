4fc428611fdbf6539be9c4f024a3247f06893720
AlexAegis <alexaegis@gmail.com>
2020-12-11 08:20:25 +0100

    chore(typescript): added timeout

--- src/2020/day11/typescript/part_two.spec.ts
+++ src/2020/day11/typescript/part_two.spec.ts
@@ -6,9 +6,9 @@ import { runner } from './part_two';
 describe(`2020 - Day 11 - Part Two`, () => {
 	it(`should solve the input`, async () => {
 		expect(runner((await read(year, day)()).input)).to.equal(2149);
-	});
+	}).timeout(20000);
 
 	it('should solve the first example', async () => {
 		expect(runner((await read(year, day, 'example.1.txt')()).input)).to.equal(26);
-	});
+	}).timeout(20000);
 });
