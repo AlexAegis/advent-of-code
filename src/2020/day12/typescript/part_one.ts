9c6482d9f8798a75b121a9aad0de27e31ca55c3d
AlexAegis <alexaegis@gmail.com>
2020-12-12 07:02:49 +0100

    feat(typescript): compiled regex

--- src/2020/day12/typescript/part_one.ts
+++ src/2020/day12/typescript/part_one.ts
@@ -35,5 +35,5 @@ export const runner = (input: string): number => {
 };
 
 if (require.main === module) {
-	(async () => console.log(`Result: ${await bench(read(year, day), runner)}`))(); // 858 ~1.2ms
+	(async () => console.log(`Result: ${await bench(read(year, day), runner)}`))(); // 858 ~0.7ms
 }
