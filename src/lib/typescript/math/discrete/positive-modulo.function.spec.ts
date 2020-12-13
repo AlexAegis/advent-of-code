87b66def5ac7a4ee77cf937fa9080b1ab8a842d2
AlexAegis <alexaegis@gmail.com>
2020-12-13 22:22:31 +0100

    test(typescript): positive module tests

--- src/lib/typescript/math/discrete/positive-modulo.function.spec.ts
+++ src/lib/typescript/math/discrete/positive-modulo.function.spec.ts
@@ -1,7 +1,14 @@
-export const posMod = (n: number, m: number): number => {
-	return ((n % m) + m) % m;
-};
+import { expect } from 'chai';
+import { posMod, posModBigInt } from './positive-modulo.function';
 
-export const posModBigInt = (n: bigint, m: bigint): bigint => {
-	return ((n % m) + m) % m;
-};
+describe(`Positive Modulo`, () => {
+	it(`should return 2 with 2 and 4`, async () => {
+		expect(posMod(2, 4)).to.equal(2);
+		expect(posModBigInt(2n, 4n)).to.equal(2n);
+	});
+
+	it(`should return 1 with -3 and 4`, async () => {
+		expect(posMod(-3, 4)).to.equal(1);
+		expect(posModBigInt(-3n, 4n)).to.equal(1n);
+	});
+});
