24f57ead2f50234dfc4ba97d13248065964d1de5
AlexAegis <alexaegis@gmail.com>
2020-12-13 22:22:31 +0100

    test(typescript): positive module tests

--- src/lib/typescript/math/discrete/positive-modulo.function.ts
+++ src/lib/typescript/math/discrete/positive-modulo.function.ts
@@ -1,8 +1,18 @@
+/**
+ * Modulo normalizing to a positive remainer
+ *
+ * @returns the positive remainder
+ */
 export const posMod = (n: number, m: number): number => {
 	const mod = n % m;
 	return mod >= 0 ? mod : (mod + m) % m;
 };
 
+/**
+ * Modulo normalizing to a positive remainer
+ *
+ * @returns the positive remainder
+ */
 export const posModBigInt = (n: bigint, m: bigint): bigint => {
 	const mod = n % m;
 	return mod >= 0 ? mod : (mod + m) % m;
