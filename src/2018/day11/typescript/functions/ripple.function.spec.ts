6819702c05720559ebdc1b087b8969ba22e000a5
AlexAegis <alexaegis@gmail.com>
2020-12-18 22:30:27 +0100

    refactor(typescript): 2018 day 11 libvec

--- src/2018/day11/typescript/functions/ripple.function.spec.ts
+++ src/2018/day11/typescript/functions/ripple.function.spec.ts
@@ -1,7 +1,7 @@
 import { expect } from 'chai';
 import { ripple } from '../functions/ripple.function';
 
-describe('Day 11, ripple', () => {
+describe('Day 11 - Ripple', () => {
 	it('Should be only 40 of them:', () => {
 		expect(ripple(5, 3, 4).length).to.equal(40);
 	});
--- src/2018/day11/typescript/functions/ripple.function.ts
+++ src/2018/day11/typescript/functions/ripple.function.ts
@@ -1,15 +1,15 @@
-import { Coord } from '../model/coord.class';
+import { Vec2 } from '@lib/model';
 
-export const ripple = (radius: number, xoffset = 0, yoffset = 0): Coord[] => {
+export const ripple = (radius: number, xoffset = 0, yoffset = 0): Vec2[] => {
 	const result = [];
 	if (radius === 0) {
-		result.push(new Coord(xoffset, yoffset));
+		result.push(new Vec2(xoffset, yoffset));
 	}
 	for (let i = -radius; i < radius; i++) {
-		result.push(new Coord(-radius + xoffset, 1 + i + yoffset)); // top
-		result.push(new Coord(radius + xoffset, i + yoffset)); // bottom
-		result.push(new Coord(i + xoffset, -radius + yoffset)); // left
-		result.push(new Coord(i + xoffset + 1, radius + yoffset)); // right
+		result.push(new Vec2(-radius + xoffset, 1 + i + yoffset)); // top
+		result.push(new Vec2(radius + xoffset, i + yoffset)); // bottom
+		result.push(new Vec2(i + xoffset, -radius + yoffset)); // left
+		result.push(new Vec2(i + xoffset + 1, radius + yoffset)); // right
 	}
 	return result;
 };
