dd63b4f58b9c22f0e52a2e552dadd9a77b66c6dd
AlexAegis <alexaegis@gmail.com>
2020-12-18 22:30:27 +0100

    refactor(typescript): 2018 day 11 libvec

--- src/2018/day11/typescript/functions/range.generator.ts
+++ src/2018/day11/typescript/functions/range.generator.ts
@@ -1,14 +1,14 @@
-import { Coord } from '../model/coord.class';
+import { Vec2 } from '@lib/model';
 
 export interface Range {
 	from: number;
 	to: number;
 }
 
-export function* range(xRange: Range, yRange: Range): Generator<Coord> {
+export function* range(xRange: Range, yRange: Range): Generator<Vec2> {
 	for (let x = xRange.from; x <= xRange.to; x++) {
 		for (let y = yRange.from; y <= yRange.to; y++) {
-			yield new Coord(x, y);
+			yield new Vec2(x, y);
 		}
 	}
 }
