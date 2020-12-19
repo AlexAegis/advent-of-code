efc4cc8ef3674f8e21bc69855ef4b80ef59640c6
AlexAegis <alexaegis@gmail.com>
2020-12-19 09:19:58 +0100

    feat(typescript): added memoize function

--- src/lib/typescript/functions/memoize.function.ts
+++ src/lib/typescript/functions/memoize.function.ts
@@ -1,6 +1,20 @@
 /**
- * TODO
+ * Can cache results for pure functions
+ *
+ * @param f to memoize
  */
-export const memoize = (): void => {
-	console.log('TODO');
-};
+export const memoize = <F extends (...args: never[]) => ReturnType<F>>(
+	f: F,
+	cache = new Map<string, ReturnType<F>>()
+): F =>
+	((...args: never[]) => {
+		const hash = JSON.stringify(args);
+		const cached = cache.get(hash);
+		if (cached) {
+			return cached;
+		} else {
+			const result = f(...args);
+			cache.set(hash, result);
+			return result;
+		}
+	}) as F;
