83753f4fc294d3dc55e7683971854367ee8083bb
AlexAegis <alexaegis@gmail.com>
2020-12-25 07:36:25 +0100

    refactor: moved parse out

--- src/2020/day21/typescript/part_two.spec.ts
+++ src/2020/day21/typescript/part_two.spec.ts
@@ -5,10 +5,10 @@ import { runner } from './part_two';
 
 describe('2020 - Day 21 - Part Two', () => {
 	it('should solve the input', async () => {
-		expect(runner((await read(year, day)()).input)).to.equal(224);
+		expect(runner((await read(year, day)()).input)).to.equal(0);
 	});
 
 	it('should solve the first example', async () => {
-		expect(runner((await read(year, day, 'example.1.txt')()).input)).to.equal(2);
+		expect(runner((await read(year, day, 'example.1.txt')()).input)).to.equal(0);
 	});
 });
--- src/2020/day21/typescript/part_two.ts
+++ src/2020/day21/typescript/part_two.ts
@@ -1,16 +1,10 @@
-import { bench, read, split } from '@lib';
+import { bench, read } from '@lib';
 import { day, year } from '.';
+import { parse } from './parse.function';
 
 export const runner = (input: string): number => {
-	const lines = split(input);
-
-	for (const line of lines) {
-		const [rawIngredients, rawAllergens] = line.split(' (contains ');
-		const ingredients = rawIngredients.split(' ');
-		const allergens = rawAllergens.substring(0, rawAllergens.length - 1).split(', ');
-		console.log('ing', JSON.stringify(ingredients), 'all', JSON.stringify(allergens));
-	}
-
+	const entries = parse(input);
+	console.log(entries);
 	return 0;
 };
 
