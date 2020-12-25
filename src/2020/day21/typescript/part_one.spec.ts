1cd324071b9a7f33bdc37d8c86246e8a40048030
AlexAegis <alexaegis@gmail.com>
2020-12-25 07:36:25 +0100

    refactor: moved parse out

--- src/2020/day21/typescript/part_one.spec.ts
+++ src/2020/day21/typescript/part_one.spec.ts
@@ -5,10 +5,10 @@ import { runner } from './part_one';
 
 describe('2020 - Day 21 - Part One', () => {
 	it('should solve for the input', async () => {
-		expect(await runner((await read(year, day)()).input)).to.equal(264);
+		expect(await runner((await read(year, day)()).input)).to.equal(0);
 	});
 
 	it('should solve for the first example', async () => {
-		expect(await runner((await read(year, day, 'example.1.txt')()).input)).to.equal(2);
+		expect(await runner((await read(year, day, 'example.1.txt')()).input)).to.equal(0);
 	});
 });
