b97ca8c41b509b8a0984152b53552f844bfa54e8
AlexAegis <alexaegis@gmail.com>
2021-01-08 13:06:56 +0100

    feat(typescript): added rightSplit

---
+++ src/lib/typescript/string/right-split.function.spec.ts
@@ -0,0 +1,22 @@
+import { expect } from 'chai';
+import { rightSplit } from './right-split.function';
+
+describe('rightSplit', () => {
+	it('should be able to split a string apart', () => {
+		const [a, b] = rightSplit('a b c');
+		expect(a).to.equal('a b');
+		expect(b).to.equal('c');
+	});
+
+	it('should return the original string if the delimiter does not exist', () => {
+		const [a, b] = rightSplit('a b c', 'x');
+		expect(a).to.equal('a b c');
+		expect(b).to.be.undefined;
+	});
+
+	it('should return the original string if the delimiter is falsy', () => {
+		const [a, b] = rightSplit('a b c', '');
+		expect(a).to.equal('a b c');
+		expect(b).to.be.undefined;
+	});
+});
--- src/lib/typescript/string/string.polyfill.ts
+++ src/lib/typescript/string/string.polyfill.ts
@@ -1,5 +1,6 @@
 import { Vec2 } from '@lib/model';
 import { NEWLINE } from '@lib/regex';
+import { rightSplit } from './right-split.function';
 import { stringToMatrix } from './string-to-matrix.function';
 import { vectorsInStringTile } from './vectors-in-string-tile.function';
 
@@ -7,6 +8,7 @@ declare global {
 	interface String {
 		toMatrix(): string[][];
 		vectorsOf(character: string, fromBottom?: boolean): Vec2[];
+		rightSplit(delimiter?: string): [string, string] | [string];
 	}
 }
 
@@ -19,4 +21,8 @@ String.prototype.toMatrix = function (
 
 String.prototype.vectorsOf = function (character: string, fromBottom = false): Vec2[] {
 	return vectorsInStringTile(this as string, character, fromBottom);
+};
+
+String.prototype.rightSplit = function (delimiter = ' '): [string, string] | [string] {
+	return rightSplit(this as string, delimiter);
 };
