80332028e60677beedd787cc3d76036ec9119638
AlexAegis <alexaegis@gmail.com>
2021-01-10 15:42:05 +0100

    feat(rust): updated packages

--- solutions/rust/2015/01/tests/part_2.rs
+++ solutions/rust/2015/01/tests/part_2.rs
@@ -1,20 +1,22 @@
 use aoclib::Solvable;
 
+use aoc201501::part_2::PartTwo;
+
 #[test]
 fn input() -> aoclib::Result<()> {
 	let input = aoclib::reader(2015, 1, "input.txt")?;
-	assert_eq!(aoc201501::PartTwo::solve(&input)?, 1795);
+	assert_eq!(PartTwo::solve(&input)?, 1795);
 	Ok(())
 }
 
 #[test]
 fn example_1() -> aoclib::Result<()> {
-	assert_eq!(aoc201501::PartTwo::solve(")")?, 1);
+	assert_eq!(PartTwo::solve(")")?, 1);
 	Ok(())
 }
 
 #[test]
 fn example_2() -> aoclib::Result<()> {
-	assert_eq!(aoc201501::PartTwo::solve("()())")?, 5);
+	assert_eq!(PartTwo::solve("()())")?, 5);
 	Ok(())
 }
