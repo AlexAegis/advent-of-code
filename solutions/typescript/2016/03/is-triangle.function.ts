4d7a70f207f8a27e63fb696530ebee054cf226db
AlexAegis <alexaegis@gmail.com>
2021-11-14 23:27:31 +0100

    feat: simplified

--- solutions/typescript/2016/03/is-triangle.function.ts
+++ solutions/typescript/2016/03/is-triangle.function.ts
@@ -1,8 +1,3 @@
 export type Triangle = [number, number, number];
 
-export const isTriangle = ([a, b, c]: [number, number, number]): boolean => {
-	const ab = a + b;
-	const bc = b + c;
-	const ca = c + a;
-	return ab > c && bc > a && ca > b;
-};
+export const isTriangle = ([a, b, c]: Triangle): boolean => a + b > c && b + c > a && c + a > b;
