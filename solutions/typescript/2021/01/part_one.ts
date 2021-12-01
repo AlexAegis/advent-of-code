1751b49598fc1bf954831b293f80cd9812d9f988
AlexAegis <alexaegis@gmail.com>
2021-12-01 06:51:21 +0100

    feat(typescript): simplify 2021 day 1 solution

--- solutions/typescript/2021/01/part_one.ts
+++ solutions/typescript/2021/01/part_one.ts
@@ -1,20 +1,13 @@
 import { bench, read } from '@lib';
 import { day, year } from '.';
 
-export const runner = (input: string): number => {
-	const data = input.splitToInt();
-	let prev = undefined;
-	let inc = 0;
-	for (let i = 0; i < data.length; i++) {
-		const element = data[i];
-		if (prev && prev < element) {
-			inc++;
-		}
-		prev = element;
-	}
-	return inc;
-};
+export const runner = (input: string): number =>
+	input
+		.splitToInt()
+		.slideWindow()
+		.filter(([a, b]) => a < b).length;
+
 // istanbul ignore next
 if (require.main === module) {
-	(async () => console.log(`Result: ${await bench(read(year, day), runner)}`))(); // 1451 ~0.15ms
+	(async () => console.log(`Result: ${await bench(read(year, day), runner)}`))(); // 1451 ~0.33ms
 }
