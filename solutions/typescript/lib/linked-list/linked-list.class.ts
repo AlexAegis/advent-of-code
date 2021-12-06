81183e000b0d22c58d127c76e73eeec663d78659
AlexAegis <alexaegis@gmail.com>
2021-12-06 07:59:16 +0100

    feat(typescript): added length tracking for LinkedList

--- solutions/typescript/lib/linked-list/linked-list.class.ts
+++ solutions/typescript/lib/linked-list/linked-list.class.ts
@@ -4,22 +4,32 @@ import { LinkedListNode } from './linked-list-node.class';
 export class LinkedList<T> implements ToString {
 	public start?: LinkedListNode<T>;
 	public end?: LinkedListNode<T>;
+
+	#length = 0;
+
 	public constructor(...initialData: T[]) {
 		for (const item of initialData) {
 			this.add(item);
 		}
 	}
 
-	public add(t: T): void {
-		if (!this.start || !this.end) {
-			this.start = new LinkedListNode(t);
-			this.end = this.start;
-		} else {
-			const link = new LinkedListNode(t);
-			link.prev = this.end;
-			this.end.next = link;
-			this.end = link;
+	public add(...ts: T[]): void {
+		for (const t of ts) {
+			if (!this.start || !this.end) {
+				this.start = new LinkedListNode(t);
+				this.end = this.start;
+			} else {
+				const link = new LinkedListNode(t);
+				link.prev = this.end;
+				this.end.next = link;
+				this.end = link;
+			}
+			this.#length++;
 		}
+	}
+
+	get length(): number {
+		return this.#length;
 	}
 
 	*[Symbol.iterator](): IterableIterator<T> {
