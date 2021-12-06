344cd43545bf666e2b443e5622a49150ff7130fd
AlexAegis <alexaegis@gmail.com>
2021-12-06 07:59:59 +0100

    feat(typescript): added change function to Map

--- solutions/typescript/lib/map/map.polyfill.ts
+++ solutions/typescript/lib/map/map.polyfill.ts
@@ -3,8 +3,18 @@ declare global {
 		getOrAdd(key: K, creator: (key: K) => V): V;
 		isTheSameAs(other: Map<K, V>): boolean;
 		findKey(value: V): K | undefined;
+		change(key: K, change: (value: V) => V, initialize: () => V): void;
 	}
 }
+
+Map.prototype.change = function <K, V>(key: K, change: (value: V) => V, initialize: () => V): void {
+	const existing = this.get(key);
+	if (existing) {
+		this.set(key, change(existing));
+	} else {
+		this.set(key, initialize());
+	}
+};
 
 Map.prototype.findKey = function <K, V>(value: V): K | undefined {
 	return [...this.entries()].find((e) => e[1] === value) as K | undefined;
