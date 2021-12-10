6edab1ec4d4651020833182906f2615575140c07
AlexAegis <alexaegis@gmail.com>
2021-12-10 07:25:19 +0100

    feat(typescript): add identity function

---
+++ solutions/typescript/lib/functions/identity.function.spec.ts
@@ -0,0 +1,20 @@
+import { expect } from 'chai';
+import { identity } from './identity.function';
+
+describe('identity', () => {
+	it('should be true for a non-nullish value', () => {
+		expect(identity(1)).to.be.true;
+	});
+
+	it('should be true for a nullish but non-null value', () => {
+		expect(identity(0)).to.be.true;
+	});
+
+	it('should be false for undefined', () => {
+		expect(identity(undefined)).to.be.false;
+	});
+
+	it('should be false for null', () => {
+		expect(identity(null)).to.be.false;
+	});
+});
