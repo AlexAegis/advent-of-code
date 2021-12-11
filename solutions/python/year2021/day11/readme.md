e8e913a644fa7850ea7dec35bdb87990ee81034b
AlexAegis <alexaegis@gmail.com>
2021-12-11 07:00:13 +0100

    feat(typescript): solved 2021 day 11

---
+++ solutions/python/year2021/day11/readme.md
@@ -0,0 +1,1 @@
+../../../../resources/2021/11/readme.md
---
+++ solutions/typescript/2021/11/bench.ts
@@ -0,0 +1,18 @@
+import { read } from '@lib';
+import { defaultBench } from '@lib/benchmark';
+import { add } from 'benny';
+import { day, year } from '.';
+import { runner as partOneRunner } from './part_one';
+import { runner as partTwoRunner } from './part_two';
+
+defaultBench(
+	'2021 - Day 11',
+	add('Part One', async () => {
+		const input = (await read(year, day)()).input;
+		return () => partOneRunner(input);
+	}),
+	add('Part Two', async () => {
+		const input = (await read(year, day)()).input;
+		return () => partTwoRunner(input);
+	})
+);
