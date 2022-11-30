f43801acaa1bbd63a90f4bcdbf56ebd3536d383a
AlexAegis <alexaegis@gmail.com>
2022-11-30 21:22:46 +0100

    fix: readme url to ts solutions turbo cache added 2022 day 1 stub

---
+++ solutions/typescript/2022/01/src/p2.ts
@@ -0,0 +1,13 @@
+import { bench, read } from '@alexaegis/advent-of-code-lib';
+import packageJson from '../package.json' assert { type: 'json' };
+
+export const p2 = (input: string): number => {
+	const values = input.splitToInt();
+	console.log('values', values);
+	return 0;
+};
+
+if (process.env.RUN) {
+	const input = await read(packageJson.aoc.year, packageJson.aoc.day);
+	console.log(`Result: ${await bench(input, p2)}`); // 0 ~0ms
+}
