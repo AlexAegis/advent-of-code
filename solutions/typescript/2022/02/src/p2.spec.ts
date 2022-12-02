1c1633d9794ee2e6f23f43e6a1226d5d915148d7
AlexAegis <alexaegis@gmail.com>
2022-12-02 05:40:47 +0100

    chore: prepare 2022 02

---
+++ solutions/typescript/2022/02/src/p2.spec.ts
@@ -0,0 +1,20 @@
+import { loadTaskResources } from '@alexaegis/advent-of-code-lib';
+import { describe, expect, it } from 'vitest';
+import packageJson from '../package.json' assert { type: 'json' };
+import { p2 } from './p2.js';
+
+describe('2022 02 p2', () => {
+	describe('the input', () => {
+		it('should solve the input', async () => {
+			const { input } = await loadTaskResources(packageJson.aoc);
+			expect(p2(input)).to.equal(199357);
+		});
+	});
+
+	describe('example 1', () => {
+		it('should be solved', async () => {
+			const { input } = await loadTaskResources(packageJson.aoc, 'example.1.txt');
+			expect(p2(input)).to.equal(45000);
+		});
+	});
+});
