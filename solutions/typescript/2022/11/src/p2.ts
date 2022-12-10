d2361d3872f7fcea6f154b81671641c92fe778db
AlexAegis <alexaegis@gmail.com>
2022-12-10 23:42:04 +0100

    chore: prepare 2022 11

---
+++ solutions/typescript/2022/11/src/p2.ts
@@ -0,0 +1,11 @@
+import { split, task } from '@alexaegis/advent-of-code-lib';
+import packageJson from '../package.json' assert { type: 'json' };
+
+export const p2 = (input: string): number => {
+	const lines = split(input);
+
+	console.log(lines);
+	return 0;
+};
+
+await task(p2, packageJson.aoc); // 0 ~0ms
