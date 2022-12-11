03c0b5d9327a653a972416ed4e2e8c7c59c50196
AlexAegis <alexaegis@gmail.com>
2022-12-11 17:53:40 +0100

    chore: prepare the rest of 2022 tasks

---
+++ solutions/typescript/2022/23/src/bench.ts
@@ -0,0 +1,18 @@
+import { loadTaskResources } from '@alexaegis/advent-of-code-lib';
+import { defaultBench } from '@alexaegis/advent-of-code-lib/benchmark';
+import { add } from 'benny';
+import packageJson from '../package.json' assert { type: 'json' };
+import { p1 } from './p1.js';
+import { p2 } from './p2.js';
+
+defaultBench(
+	'2022 - Day 23',
+	add('Part One', async () => {
+		const { input } = await loadTaskResources(packageJson.aoc);
+		return () => p1(input);
+	}),
+	add('Part Two', async () => {
+		const { input } = await loadTaskResources(packageJson.aoc);
+		return () => p2(input);
+	})
+);
---
+++ solutions/typescript/2022/24/src/p1.spec.ts
@@ -0,0 +1,20 @@
+import { loadTaskResources } from '@alexaegis/advent-of-code-lib';
+import { describe, expect, it } from 'vitest';
+import packageJson from '../package.json' assert { type: 'json' };
+import { p1 } from './p1.js';
+
+describe('2022 24 p1', () => {
+	describe('the input', () => {
+		it('should solve the input', async () => {
+			const { input } = await loadTaskResources(packageJson.aoc);
+			expect(p1(input)).to.equal(0);
+		});
+	});
+
+	describe('example 1', () => {
+		it('should be solved', async () => {
+			const { input } = await loadTaskResources(packageJson.aoc, 'example.1.txt');
+			expect(p1(input)).to.equal(0);
+		});
+	});
+});
