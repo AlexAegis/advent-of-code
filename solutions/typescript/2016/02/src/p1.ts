0773b18d830ea02c6ed6ff43187e40e52cc59add
AlexAegis <alexaegis@gmail.com>
2022-12-15 05:41:27 +0100

    chore: remaining boundingbox migration, fixed turbo tsc dep

--- solutions/typescript/2016/02/src/p1.ts
+++ solutions/typescript/2016/02/src/p1.ts
@@ -1,6 +1,10 @@
 import { split, task } from '@alexaegis/advent-of-code-lib';
-import type { BoundingBox } from '@alexaegis/advent-of-code-lib/model';
-import { Direction, isDirectionMarker, Vec2 } from '@alexaegis/advent-of-code-lib/model';
+import {
+	BoundingBox,
+	Direction,
+	isDirectionMarker,
+	Vec2,
+} from '@alexaegis/advent-of-code-lib/model';
 import packageJson from '../package.json' assert { type: 'json' };
 
 const toKeypadNumber = (vec: Vec2): number => (2 - vec.y) * 3 + vec.x + 1;
@@ -8,10 +12,7 @@ const toKeypadNumber = (vec: Vec2): number => (2 - vec.y) * 3 + vec.x + 1;
 export const p1 = (input: string): number => {
 	const position = new Vec2(1, 1);
 
-	const keypadArea: BoundingBox = {
-		topLeft: Vec2.ORIGIN,
-		bottomRight: new Vec2(2, 2),
-	};
+	const keypadArea = BoundingBox.fromVectors(Vec2.ORIGIN, new Vec2(2, 2));
 
 	const result = split(input).reduce((acc, line) => {
 		line.split('')
