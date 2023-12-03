dce1cae7892da90cf11c71e9949af4bc69b6de0c
AlexAegis <alexaegis@gmail.com>
2023-12-03 06:52:28 +0100

    feat: initial solution for 2023 day 3

--- solutions/typescript/2023/03/src/p2.spec.ts
+++ solutions/typescript/2023/03/src/p2.spec.ts
@@ -7,14 +7,14 @@ describe('2023 03 p2', () => {
 	describe('the input', () => {
 		it('should solve the input', async () => {
 			const { input } = await loadTaskResources(packageJson.aoc);
-			expect(p2(input)).toEqual(0);
+			expect(p2(input)).toEqual(82_818_007);
 		});
 	});
 
 	describe('example 1', () => {
 		it('should be solved', async () => {
 			const { input } = await loadTaskResources(packageJson.aoc, 'example.1.txt');
-			expect(p2(input)).toEqual(0);
+			expect(p2(input)).toEqual(467_835);
 		});
 	});
 });
--- solutions/typescript/2023/03/src/p2.ts
+++ solutions/typescript/2023/03/src/p2.ts
@@ -1,8 +1,60 @@
-import { task } from '@alexaegis/advent-of-code-lib';
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
+/* eslint-disable @typescript-eslint/prefer-for-of */
+import { Direction, Vec2, task } from '@alexaegis/advent-of-code-lib';
 import packageJson from '../package.json';
 
 export const p2 = (input: string): number => {
-	return input.lines().length;
+	const m = input.toMatrix();
+	const numbers: number[] = [];
+	let num: string | undefined = undefined;
+	let lastVecs: Vec2[] = [];
+
+	const gears = new Map<string, number[]>();
+	for (let y = 0; y < m.length; y++) {
+		const row = m[y]!;
+
+		for (let x = 0; x < row.length; x++) {
+			const v = row[x]!;
+
+			const gearVec = lastVecs
+				.map((lv) =>
+					Direction.allDirections
+						.map((d) => d.add(lv))
+						.find((v) => {
+							// const cv = new Vec2(x, i);
+							const n = m[v.y]?.[v.x];
+							if (n) {
+								// console.log(num, n, v.toString());
+							}
+							return n && n === '*';
+						}),
+				)
+				.find((v) => !!v);
+
+			if (/\d/.test(v)) {
+				num = num === undefined ? v : num + v;
+				lastVecs.push(new Vec2({ x, y: y }));
+			} else if (num !== undefined && gearVec) {
+				//TODO check surr
+				numbers.push(num.toInt());
+				if (gears.has(gearVec.toString())) {
+					gears.get(gearVec.toString())?.push(num.toInt());
+				} else {
+					gears.set(gearVec.toString(), [num.toInt()]);
+				}
+				num = undefined;
+				lastVecs = [];
+			} else {
+				num = undefined;
+				lastVecs = [];
+			}
+		}
+	}
+
+	return [...gears.values()]
+		.filter((gp) => gp.length === 2)
+		.map((gp) => gp.product())
+		.sum();
 };
 
-await task(p2, packageJson.aoc); // 0 ~0.22ms
+await task(p2, packageJson.aoc); // 82818007 ~3.89ms
