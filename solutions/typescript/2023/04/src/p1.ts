2f5acddcc670c129a2c3d8852ad4af98a790ab0b
AlexAegis <alexaegis@gmail.com>
2023-12-04 02:26:12 +0100

    chore: prepare 2023 day 4

---
+++ solutions/typescript/2023/04/src/p1.ts
@@ -0,0 +1,9 @@
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
+import { task } from '@alexaegis/advent-of-code-lib';
+import packageJson from '../package.json';
+
+export const p1 = (_input: string): number => {
+	return 0;
+};
+
+await task(p1, packageJson.aoc); // 539637 ~1.39ms
---
+++ solutions/typescript/2023/04/src/p2.ts
@@ -0,0 +1,9 @@
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
+import { task } from '@alexaegis/advent-of-code-lib';
+import packageJson from '../package.json';
+
+export const p2 = (_input: string): number => {
+	return 1;
+};
+
+await task(p2, packageJson.aoc); // 82818007 ~3.89ms
