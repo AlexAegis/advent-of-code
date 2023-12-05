237b8b36f6f3c1c42c48c2a963f936b2e4fedaff
AlexAegis <alexaegis@gmail.com>
2023-12-05 07:02:50 +0100

    feat: 2023 day 5 part 1

--- solutions/typescript/2023/05/src/p1.ts
+++ solutions/typescript/2023/05/src/p1.ts
@@ -1,12 +1,21 @@
 import { task } from '@alexaegis/advent-of-code-lib';
 import packageJson from '../package.json';
-import { parse } from './parse.js';
+import { findRange, parse } from './parse.js';
 
 export const p1 = (input: string): number => {
 	const data = parse(input);
-	console.log(data);
-	return 0;
+	return data.seeds
+		.map((seed) => {
+			const soil = findRange(seed, data.seedToSoilMap);
+			const fertilizer = findRange(soil, data.soilToFertilizerMap);
+			const water = findRange(fertilizer, data.fertilizerToWaterMap);
+			const light = findRange(water, data.waterToLightMap);
+			const temperature = findRange(light, data.lightToTemperatureMap);
+			const humidity = findRange(temperature, data.temperatureToHumidityMap);
+			const location = findRange(humidity, data.humidityToLocationMap);
+			return location;
+		})
+		.min();
 };
 
-await task(p1, packageJson.aoc, 'example.1.txt'); // 0 ~4.36ms
-// await task(p1, packageJson.aoc); // 0 ~4.36ms
+await task(p1, packageJson.aoc); // 84470622 ~4.36ms
