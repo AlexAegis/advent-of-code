459a36205fae3ba7b0eba77a0ddd2450d432ada2
AlexAegis <alexaegis@gmail.com>
2023-12-08 01:04:42 +0100

    chore: fixing around in day 5

--- solutions/typescript/2023/05/src/p1.ts
+++ solutions/typescript/2023/05/src/p1.ts
@@ -1,21 +1,23 @@
 import { task } from '@alexaegis/advent-of-code-lib';
 import packageJson from '../package.json';
-import { findRange, parse } from './parse.js';
+import { findRange, parse, type Card } from './parse.js';
+
+export const mapSeed =
+	(data: Card) =>
+	(seed: number): number => {
+		const soil = findRange(seed, data.seedToSoilMap);
+		const fertilizer = findRange(soil, data.soilToFertilizerMap);
+		const water = findRange(fertilizer, data.fertilizerToWaterMap);
+		const light = findRange(water, data.waterToLightMap);
+		const temperature = findRange(light, data.lightToTemperatureMap);
+		const humidity = findRange(temperature, data.temperatureToHumidityMap);
+		const location = findRange(humidity, data.humidityToLocationMap);
+		return location;
+	};
 
 export const p1 = (input: string): number => {
 	const data = parse(input);
-	return data.seeds
-		.map((seed) => {
-			const soil = findRange(seed, data.seedToSoilMap);
-			const fertilizer = findRange(soil, data.soilToFertilizerMap);
-			const water = findRange(fertilizer, data.fertilizerToWaterMap);
-			const light = findRange(water, data.waterToLightMap);
-			const temperature = findRange(light, data.lightToTemperatureMap);
-			const humidity = findRange(temperature, data.temperatureToHumidityMap);
-			const location = findRange(humidity, data.humidityToLocationMap);
-			return location;
-		})
-		.min();
+	return data.seeds.map(mapSeed(data)).min();
 };
 
 await task(p1, packageJson.aoc); // 84470622 ~4.36ms
--- solutions/typescript/2023/05/src/p2.ts
+++ solutions/typescript/2023/05/src/p2.ts
@@ -1,5 +1,6 @@
 import { Interval, task } from '@alexaegis/advent-of-code-lib';
 import packageJson from '../package.json';
+import { mapSeed } from './p1.js';
 import { parse, type Range } from './parse.js';
 
 export const getRangeEnd = (range: Range): number => {
@@ -13,27 +14,49 @@ export const getRangeEnd = (range: Range): number => {
  */
 export const refract = (left: Range[], right: Range[]): Range[] => {
 	const middle = Interval.merge(left.map((l) => l.to));
+	console.log('middle', middle.toString());
 	// first, this is the set of sections that are mapping to another region
-	const results = right.filterMap<Range>((r) => {
-		const intersection = Interval.intersect([r.from, ...middle]);
-		return intersection
-			? {
-					from: intersection,
-					slope: r.slope,
-					sourceRangeStart: intersection.low,
-					destinationRange: intersection.low + r.slope,
-					rangeLength: r.rangeLength,
-					to: new Interval(intersection.low, intersection.high, {
-						highQualifier: intersection.highQualifier,
-						lowQualifier: intersection.lowQualifier,
-					}),
-			  }
-			: undefined;
+	const results = right.flatMap<Range>((r) => {
+		const intersecitons = middle.filterMap((m) => Interval.intersection(m, r.from));
+
+		return intersecitons.filterMap((intersection) => {
+			const res = intersection
+				? {
+						from: intersection,
+						slope: r.slope,
+						sourceRangeStart: intersection.low,
+						destinationRange: intersection.low + r.slope,
+						rangeLength:
+							intersection.length - (intersection.highQualifier === 'closed' ? 1 : 0),
+						to: new Interval(intersection.low + r.slope, intersection.high + r.slope, {
+							highQualifier: intersection.highQualifier,
+							lowQualifier: intersection.lowQualifier,
+						}),
+				  }
+				: undefined;
+
+			console.log(
+				'INTERSEC',
+				r.from.toString(),
+				'->',
+				r.to.toString(),
+				'intersection:',
+				intersection?.toString() ?? 'NONE',
+				'=>',
+				res?.to.toString(),
+			);
+			return res;
+		});
 	});
 
 	const nonMapping = Interval.complement(
 		results.map((r) => r.from),
-		middle,
+		middle.map((m) => Interval.open(m.low, m.high)),
+	).filter((i) => i.isFinite() && i.length > 0);
+
+	console.log(
+		'nonMapping',
+		nonMapping.map((nm) => nm.toString()),
 	);
 
 	results.push(
@@ -61,16 +84,16 @@ export const p2 = (input: string): number => {
 	}));
 
 	console.log(
-		'seedRanges',
-		seedRanges.map((r) => `from: ${r.from.toString()} to: ${r.to.toString()}`).join('\n'),
+		'seedRanges\n',
+		seedRanges.map((r) => `\t\tfrom: ${r.from.toString()} to: ${r.to.toString()}`).join('\n'),
 	);
 
 	const seedToSoilRefraction = refract(seedRanges, data.seedToSoilMap);
 
 	console.log(
-		'seedToSoilRefraction',
+		'seedToSoilRefraction\n',
 		seedToSoilRefraction
-			.map((r) => `from: ${r.from.toString()} to: ${r.to.toString()}`)
+			.map((r) => `\t\tfrom: ${r.from.toString()} to: ${r.to.toString()}`)
 			.join('\n'),
 	);
 
@@ -99,8 +122,13 @@ export const p2 = (input: string): number => {
 	);
 
 	const result = humidityToLocationRefraction.map((range) => range.sourceRangeStart).min();
-	console.log(result);
-	return 0;
+
+	const mapped = mapSeed(data)(result);
+	console.log('result', result, 'mapped', mapped);
+	return mapped;
 };
 
 await task(p2, packageJson.aoc, 'example.1.txt'); // 84470622 ~4.36ms
+
+// await task(p2, packageJson.aoc); // 84470622 ~4.36ms
+// is too high 4194961753
