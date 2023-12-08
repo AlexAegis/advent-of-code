cb93437fb8ff4ee0c582fdbb0244f034743338ae
AlexAegis <alexaegis@gmail.com>
2023-12-08 06:56:55 +0100

    feat: cleaned up 2023 day 8, added benches

--- solutions/typescript/2023/08/src/p2.ts
+++ solutions/typescript/2023/08/src/p2.ts
@@ -1,48 +1,15 @@
-/* eslint-disable @typescript-eslint/no-non-null-assertion */
-import { Direction, Graph, GraphNode, lcm, task } from '@alexaegis/advent-of-code-lib';
+import { lcm, task } from '@alexaegis/advent-of-code-lib';
 import packageJson from '../package.json';
+import { parse, stepUntil } from './parse.js';
 
 export const p2 = (input: string): number => {
-	const [rawInstructions, ...lines] = input.lines(false);
-
-	const instructions = [...(rawInstructions ?? '')];
-	const graph = new Graph<number>();
-	for (const line of lines) {
-		const [key, gates] = line.splitIntoStringPair(' = ');
-		const [leftGate, rightGate] = gates
-			.replaceAll('(', '')
-			.replaceAll(')', '')
-			.splitIntoStringPair(', ');
-		const graphNode = graph.nodes.getOrAdd(key, (key) => new GraphNode(key, 0));
-		const leftNode = graph.nodes.getOrAdd(leftGate, (key) => new GraphNode(key, 0));
-		const rightNode = graph.nodes.getOrAdd(rightGate, (key) => new GraphNode(key, 0));
-		graphNode.neighbours.set(Direction.WEST, { from: graphNode, to: leftNode });
-		graphNode.neighbours.set(Direction.EAST, { from: graphNode, to: rightNode });
-		graph.nodes.set(key, graphNode);
-	}
-
-	graph.nodes.forEach((n) => {
-		console.log(
-			n.key,
-			n.neighbourNodes.map((nn) => nn.key),
-		);
-	});
-
+	const [graph, instructions] = parse(input);
 	const startingNodes = graph.nodeValues.filter((node) => node.key.endsWith('A'));
-	const currentNodes = [...startingNodes];
-	const steps = startingNodes.map(() => 0);
-
-	for (let c = 0; c < currentNodes.length; c++) {
-		while (!currentNodes[c]!.key.endsWith('Z')) {
-			const instruction = instructions[steps[c]! % instructions.length];
-			currentNodes[c] = currentNodes[c]!.neighbours.get(
-				instruction === 'L' ? Direction.WEST : Direction.EAST,
-			)!.to;
-			steps[c] = steps[c]! + 1;
-		}
-	}
-
-	return lcm(steps);
+	return lcm(
+		startingNodes.map((startingNode) =>
+			stepUntil(startingNode, instructions, (node) => node.key.endsWith('Z')),
+		),
+	);
 };
 
-await task(p2, packageJson.aoc); // 13334102464297 ~0ms
+await task(p2, packageJson.aoc); // 13334102464297 ~11.44ms
---
+++ solutions/typescript/2023/08/src/parse.ts
@@ -0,0 +1,41 @@
+import { Direction, Graph, GraphNode } from '@alexaegis/advent-of-code-lib';
+
+export const parse = (input: string): [Graph<number>, string[]] => {
+	const [rawInstructions, ...lines] = input.lines(false);
+	const instructions = [...(rawInstructions ?? '')];
+
+	const graph = new Graph<number>();
+	for (const line of lines) {
+		const [key, gates] = line.splitIntoStringPair(' = ');
+		const [leftGate, rightGate] = gates
+			.replaceAll('(', '')
+			.replaceAll(')', '')
+			.splitIntoStringPair(', ');
+		const graphNode = graph.nodes.getOrAdd(key, (key) => new GraphNode(key, 0));
+		const leftNode = graph.nodes.getOrAdd(leftGate, (key) => new GraphNode(key, 0));
+		const rightNode = graph.nodes.getOrAdd(rightGate, (key) => new GraphNode(key, 0));
+		graphNode.neighbours.set(Direction.WEST, { from: graphNode, to: leftNode });
+		graphNode.neighbours.set(Direction.EAST, { from: graphNode, to: rightNode });
+		graph.nodes.set(key, graphNode);
+	}
+
+	return [graph, instructions];
+};
+
+export const stepUntil = (
+	fromNode: GraphNode<number>,
+	instructions: string[],
+	until: (n: GraphNode<number>) => boolean,
+): number => {
+	let step = 0;
+	let currentNode = fromNode;
+	while (!until(currentNode)) {
+		const instruction = instructions[step % instructions.length];
+		// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+		currentNode = currentNode.neighbours.get(
+			instruction === 'L' ? Direction.WEST : Direction.EAST,
+		)!.to;
+		step++;
+	}
+	return step;
+};
