9d321743264780b2e04e512c053405ac1bece538
AlexAegis <alexaegis@gmail.com>
2023-12-13 02:36:57 +0100

    feat: solved 2023 day 12 part 2

--- solutions/typescript/2023/12/src/p1.ts
+++ solutions/typescript/2023/12/src/p1.ts
@@ -1,38 +1,17 @@
 import { cartesianCombinations, task } from '@alexaegis/advent-of-code-lib';
 import packageJson from '../package.json';
-
-interface SprintLog {
-	/**
-	 * `?` unknown
-	 * `.` operational
-	 * `#` damaged
-	 */
-	log: string;
-	criteria: number[];
-	criteriaRegExp: RegExp;
-}
-
-export const criteriaToRegExp = (criteria: number[]): RegExp =>
-	new RegExp('^\\.*' + criteria.map((brokenCount) => `#{${brokenCount}}`).join('\\.+') + '\\.*$');
+import { parse } from './parse.js';
 
 function generateVariations(damagedLog: string, criteria: RegExp): string[] {
 	const variants = [...damagedLog].map((entry) => (entry === '?' ? ['#', '.'] : [entry]));
-	// const variationCount = variants.map((ev) => ev.length).product();
 	return cartesianCombinations(...variants)
 		.map((combination) => combination.join(''))
 		.filter((combination) => criteria.test(combination));
 }
 
 export const p1 = (input: string): number =>
-	input
-		.lines(false)
-		.map((line) => {
-			const [log, critRaw] = line.splitIntoStringPair(' ');
-			const crit = critRaw.splitToInt({ delimiter: /,/ });
-			return { log, criteria: crit, criteriaRegExp: criteriaToRegExp(crit) } as SprintLog;
-		})
+	parse(input)
 		.map((spring) => generateVariations(spring.log, spring.criteriaRegExp).length)
-
 		.sum();
 
 await task(p1, packageJson.aoc); // 7118 ~0ms
