a8eef41cb24b21aa41a16c66bf4b3efed6fb3da2
AlexAegis <alexaegis@gmail.com>
2023-12-13 02:48:20 +0100

    test: added results to the test suite

--- solutions/typescript/2023/12/src/p2.spec.ts
+++ solutions/typescript/2023/12/src/p2.spec.ts
@@ -7,14 +7,14 @@ describe('2023 12 p2', () => {
 	describe('the input', () => {
 		it('should solve the input', async () => {
 			const { input } = await loadTaskResources(packageJson.aoc);
-			expect(p2(input)).toEqual(0);
+			expect(p2(input)).toEqual(7_030_194_981_795);
 		});
 	});
 
 	describe('example 1', () => {
 		it('should be solved', async () => {
 			const { input } = await loadTaskResources(packageJson.aoc, 'example.1.txt');
-			expect(p2(input)).toEqual(0);
+			expect(p2(input)).toEqual(525_152);
 		});
 	});
 });
