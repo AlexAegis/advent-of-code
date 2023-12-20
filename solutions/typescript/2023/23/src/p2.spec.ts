7dd41e8586a39bcd665c6b5d596db2468c41b960
AlexAegis <alexaegis@gmail.com>
2023-12-20 01:42:16 +0100

    chore: prepared 2023 day 23

---
+++ solutions/typescript/2023/23/src/p2.spec.ts
@@ -0,0 +1,20 @@
+import { loadTaskResources } from '@alexaegis/advent-of-code-lib';
+import { describe, expect, it } from 'vitest';
+import packageJson from '../package.json';
+import { p2 } from './p2.js';
+
+describe.skip('2023 23 p2', () => {
+	describe('the input', () => {
+		it('should solve the input', async () => {
+			const { input } = await loadTaskResources(packageJson.aoc);
+			expect(p2(input)).toEqual(0);
+		});
+	});
+
+	describe('example 1', () => {
+		it('should be solved', async () => {
+			const { input } = await loadTaskResources(packageJson.aoc, 'example.1.txt');
+			expect(p2(input)).toEqual(0);
+		});
+	});
+});
