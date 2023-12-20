d9c4f8ad4ac102bc3fc348ab1cfb1a5a4b45ad7e
AlexAegis <alexaegis@gmail.com>
2023-12-20 01:41:08 +0100

    chore: prepared 2023 day 22

---
+++ solutions/typescript/2023/22/readme.md
@@ -0,0 +1,9 @@
+# [Day 22](https://adventofcode.com/2023/day/22)
+
+## [Part One](https://adventofcode.com/2023/day/22#part1)
+
+> [TypeScript](/solutions/typescript/2023/22/src/p1.ts)
+
+## [Part Two](https://adventofcode.com/2023/day/22#part2)
+
+> [TypeScript](/solutions/typescript/2023/22/src/p2.ts)
---
+++ solutions/typescript/2023/22/src/p1.spec.ts
@@ -0,0 +1,20 @@
+import { loadTaskResources } from '@alexaegis/advent-of-code-lib';
+import { describe, expect, it } from 'vitest';
+import packageJson from '../package.json';
+import { p1 } from './p1.js';
+
+describe.skip('2023 22 p1', () => {
+	describe('the input', () => {
+		it('should solve the input', async () => {
+			const resources = await loadTaskResources(packageJson.aoc);
+			expect(p1(resources.input)).toEqual(0);
+		});
+	});
+
+	describe('example 1', () => {
+		it('should be solved', async () => {
+			const resources = await loadTaskResources(packageJson.aoc, 'example.1.txt');
+			expect(p1(resources.input)).toEqual(0);
+		});
+	});
+});
